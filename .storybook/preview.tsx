82270d99fb770fed17dd48c13048d958b9c23b97
ACoolmanTelicent <a.coleman@telicent.io>
2024-03-06 13:41:22 +0000

    update: needed to run

---
+++ .storybook/preview.tsx
@@ -0,0 +1,42 @@
+import { Parameters, Preview } from "@storybook/react";
+import { themes } from "@storybook/theming";
+import React from "react";
+import TeliStyledEngineProvider from "../src/providers/TeliStyledEngineProvider/TeliStyledEngineProvider";
+import { DocsContainer } from "./DocsContainer";
+import ThemeWrapper from "./ThemeWrapper";
+import "../src/main.css";
+import "../src/assets/fonts/fontawesome/css/all.min.css";
+
+const decorators = [
+  (Story) => <TeliStyledEngineProvider>{Story()}</TeliStyledEngineProvider>,
+  (Story) => <ThemeWrapper>{Story()}</ThemeWrapper>,
+];
+
+const parameters: Parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      // commenting this out for now as I think its a bug in storybook 7 - Alecs
+      // color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  darkMode: {
+    classTarget: "html",
+    stylePreview: true,
+    dark: { ...themes.dark, appBg: "#1D1D1D", appContentBg: "#1D1D1D" },
+    light: { ...themes.light, appBg: "#F2F2F2", appContentBg: "#F2F2F2" },
+  },
+  a11y: {
+    element: "#storybook-root",
+    manual: false,
+  },
+  docs: { container: DocsContainer },
+};
+
+const preview: Preview = {
+  decorators,
+  parameters,
+};
+
+export default preview;
