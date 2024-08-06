27bf0b237d9505c1ede871736089362b5aa48280
Alecsandria Missangu <a_missangu@hotmail.com>
2024-08-06 10:59:57 +0100

    feat: updates to theme to accomodate user portal app
    telfe-400

--- src/v1/theme/colors/theme-colors.ts
+++ src/v1/theme/colors/theme-colors.ts
@@ -1,9 +1,25 @@
+import { common } from "@mui/material/colors";
+import { alpha, getContrastRatio } from "@mui/material/styles";
 import DATA_NAVY from "./data-navy";
 
-export type UITheme = "DataNavy";
+export type UITheme = "light" | "DataNavy";
+
+const lightMain = common.white;
 
 const THEME_COLORS = {
-  DataNavy: DATA_NAVY,
+  DataNavy: {
+    ...DATA_NAVY,
+    main: DATA_NAVY[500],
+    light: DATA_NAVY[400],
+    dark: DATA_NAVY[600],
+    contrastText: common.white,
+  },
+  light: {
+    main: lightMain,
+    light: alpha(lightMain, 0.5),
+    dark: alpha(lightMain, 0.9),
+    contrastText: "#111",
+  },
 };
 
 export default THEME_COLORS;
