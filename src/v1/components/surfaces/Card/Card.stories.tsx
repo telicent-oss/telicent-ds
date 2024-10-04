e534665629236551ff48885ec676ba9ce1e39d8b
Alecsandria Missangu <a_missangu@hotmail.com>
2024-10-04 16:30:01 +0100

    refactor: uncommented code

--- src/v1/components/surfaces/Card/Card.stories.tsx
+++ src/v1/components/surfaces/Card/Card.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 
 import Card from "./Card";
 import CardContent from "./CardContent";
-// import Text, { H1 } from "../../data-display/Text/Text";
+import Text, { H1 } from "../../data-display/Text/Text";
 import Button from "../../inputs/Button/Button";
 import CardActions from "./CardActions";
 
@@ -20,11 +20,11 @@ export const OutlinedCard: Story = {
     variant: "outlined",
     children: (
       <CardContent>
-        {/* <H1>Capybara</H1>
+        <H1>Capybara</H1>
         <Text paragraph>
           Capybara are the biggest rodents in the world. Our capy family can be seen strutting their
           stuff in our South American paddock!
-        </Text> */}
+        </Text>
         <CardActions>
           <Button variant="secondary">Share</Button>
         </CardActions>
