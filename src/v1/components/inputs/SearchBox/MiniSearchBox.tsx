beae295f106d7cc42fd67686aad6d2b62aaf3358
Alecsandria Missangu <a_missangu@hotmail.com>
2024-10-07 16:02:34 +0100

    feat: added minisearchbox and popover components
    to support the work for ticket telfe-279

---
+++ src/v1/components/inputs/SearchBox/MiniSearchBox.tsx
@@ -0,0 +1,260 @@
+import React, { useState } from "react";
+import MUICircularProgress from "@mui/material/CircularProgress";
+import OutlinedInput from "@mui/material/OutlinedInput";
+
+import { DownArrowIcon, SearchIcon } from "../../data-display";
+import { FlexBox } from "../../layout";
+import IconButton from "../Button/IconButton";
+
+export interface MiniSearchBoxRefProps
+  extends React.RefObject<HTMLButtonElement> {
+  togglePopOver?: () => void;
+}
+
+export interface SearchInputBaseProps
+  extends Omit<
+    React.HTMLAttributes<HTMLDivElement>,
+    | "children"
+    | "color"
+    | "defaultValue"
+    | "onBlur"
+    | "onChange"
+    | "onFocus"
+    | "onInvalid"
+    | "onKeyDown"
+    | "onKeyUp"
+  > {
+  /**
+   * If `true`, the `input` element is focused during the first mount.
+   */
+  autoFocus?: boolean;
+  /**
+   * If `true`, the `input` will indicate an error.
+   */
+  error?: boolean;
+  /**
+   * If `true`, the `input` will take up the full width of its container.
+   * @default false
+   */
+  fullWidth?: boolean;
+  /**
+   * The id of the `input` element.
+   */
+  id?: string;
+  /**
+   * Pass a ref to the `input` element.
+   */
+  inputRef?: React.Ref<any>;
+  /**
+   * Maximum number of rows to display when multiline option is set to true.
+   */
+  maxRows?: string | number;
+  /**
+   * Minimum number of rows to display when multiline option is set to true.
+   */
+  minRows?: string | number;
+  /**
+   * If true, a text area auto size element will be rendered
+   * @default false;
+   */
+  multiline?: boolean;
+  /**
+   * Name attribute of the `input` element.
+   */
+  name?: string;
+  /**
+   * Callback fired when the `input` is blurred.
+   *
+   * `(event: React.FocusEvent<HTMLTextAreaElement | HTMLInputElement>) => void;`
+   * Note: The first argument (event) might be undefined.
+   */
+  onBlur?: React.FocusEventHandler<HTMLInputElement | HTMLTextAreaElement>;
+  /**
+   * Callback fired when the value is changed
+   *
+   * `(event: React.ChangeEvent) => void`
+   * @param {React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>} event The event source of the callback.
+   * You can pull out the new value by accessing `event.target.value` (string).
+   */
+  onChange?: React.ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement>;
+  /**
+   * `(event: React.FocusEvent<HTMLTextAreaElement | HTMLInputElement>) => void;`
+   */
+  onFocus?: React.FocusEventHandler<HTMLInputElement | HTMLTextAreaElement>;
+  /**
+   * `(event: React.KeyboardEvent<HTMLTextAreaElement | HTMLInputElement>) => void;`
+   */
+  onKeyDown?: React.KeyboardEventHandler<
+    HTMLTextAreaElement | HTMLInputElement
+  >;
+  /**
+   * `(event: React.KeyboardEvent<HTMLTextAreaElement | HTMLInputElement>) => void;`
+   */
+  onKeyUp?: React.KeyboardEventHandler<HTMLTextAreaElement | HTMLInputElement>;
+  /**
+   * Callback fired when the `input` doesn't satisfy its constraints.
+   *
+   * `(event: React.FormEvent<HTMLTextAreaElement | HTMLInputElement>) => void;`
+   */
+  onInvalid?: React.FormEventHandler<HTMLInputElement | HTMLTextAreaElement>;
+  /**
+   * The short hint displayed in the input before the user enters a value.
+   */
+  placeholder?: string;
+  /**
+   * Number of rows to display when multiline option is set to true.
+   */
+  rows?: string | number;
+  /**
+   * The value of the `input` element, required for a controlled component.
+   */
+  value?: unknown;
+}
+
+export interface MiniSearchBoxProps extends SearchInputBaseProps {
+  /**
+   * If true, a loading indicator will be visible
+   */
+  loading?: boolean;
+  /**
+   * If true, a arrow down button will be visible
+   */
+  showDownArrowButton?: boolean;
+  /**
+   * Callback fired when the search button is clicked
+   *
+   * `(event: React.MouseEvent<HTMLButtonElement>) => void;`
+   */
+  onSearch?: React.MouseEventHandler<HTMLButtonElement>;
+  /**
+   * Callback fired when the down arrow button is clicked
+   *
+   * `(event: React.MouseEvent<HTMLButtonElement>) => void;`
+   */
+  onTogglePopOver?: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+const MiniSearchBox = (props: MiniSearchBoxProps) => {
+  const {
+    loading = false,
+    showDownArrowButton = false,
+    onSearch,
+    onTogglePopOver,
+    onFocus,
+    onBlur,
+    ...inputProps
+  } = props;
+
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleOnFocus = (
+    event: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setIsFocused(true);
+
+    if (onFocus) {
+      onFocus(event);
+    }
+  };
+
+  const handleOnBlur = (
+    event: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setIsFocused(false);
+
+    if (onBlur) {
+      onBlur(event);
+    }
+  };
+  // const internalTogglePopOverButtonRef = useRef<HTMLButtonElement>(null);
+
+  // useImperativeHandle(props.togglePopOverButtonRef, () => ({
+  //   togglePopOver: () => {
+  //     const syntheticMouseEvent = new MouseEvent("click", {
+  //       bubbles: true,
+  //       cancelable: true,
+  //       view: window,
+  //     });
+
+  //     internalToggleButtonRef.current?.dispatchEvent(syntheticMouseEvent);
+  //   },
+  // }));
+
+  // return (
+  //   <FlexBox
+  //     direction="row"
+  //     sx={{
+  //       width: 390,
+  //       borderStyle: "solid",
+  //       borderWidth: 1,
+  //       borderColor: "#989898",
+  //       borderRadius: 1,
+  //       paddingInline: 1.5,
+  //       justifyContent: "center",
+  //     }}
+  //   >
+  //     <MUIInputBase
+  //       type="search"
+  //       sx={{ width: "100%" }}
+  //       slotProps={{
+  //         input: {
+  //           sx: { padding: 0, paddingRight: 1, height: "20px" },
+  //         },
+  //       }}
+  //       // {...inputProps}
+  //     />
+  // <FlexBox direction="row" spacing={0.5}>
+  //   <IconButton size="small" onClick={onSearch} color="primary">
+  //     {loading ? (
+  //       <MUICircularProgress />
+  //     ) : (
+  //       <SearchIcon fontSize="inherit" />
+  //     )}
+  //   </IconButton>
+  //   <IconButton
+  //     // ref={internalToggleButtonRef}
+  //     size="small"
+  //     onClick={onTogglePopOver}
+  //   >
+  //     <DownArrowIcon fontSize="inherit" />
+  //   </IconButton>
+  // </FlexBox>
+  //   </FlexBox>
+  // );
+
+  return (
+    <OutlinedInput
+      size="small"
+      type="search"
+      endAdornment={
+        <>
+          <FlexBox direction="row" spacing={0.5}>
+            {loading ? (
+              <MUICircularProgress color={isFocused ? "primary" : "inherit"} size="20px" />
+            ) : (
+              <IconButton
+                size="small"
+                onClick={onSearch}
+                color={isFocused ? "primary" : undefined}
+                aria-label="search"
+              >
+                <SearchIcon fontSize="inherit" />
+              </IconButton>
+            )}
+
+            {showDownArrowButton && (
+              <IconButton size="small" aria-label="toggle pop over" onClick={onTogglePopOver}>
+                <DownArrowIcon fontSize="inherit" />
+              </IconButton>
+            )}
+          </FlexBox>
+        </>
+      }
+      onFocus={handleOnFocus}
+      onBlur={handleOnBlur}
+      {...inputProps}
+    />
+  );
+};
+
+export default MiniSearchBox;
---
+++ src/v1/components/surfaces/PopOver/PopOver.stories.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn, userEvent, within } from "@storybook/test";
+
+import Button from "../../inputs/Button/Button";
+import { Text } from "../../data-display/Text/Text";
+import PopOver from "./Popover";
+
+const meta = {
+  title: "Surfaces/PopOver",
+  component: PopOver,
+  tags: ["autodocs"],
+  args: {
+    onClose: fn(),
+  },
+} satisfies Meta<typeof PopOver>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Demo: Story = {
+  args: {
+    open: false,
+  },
+  render: (args) => {
+    const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
+      null
+    );
+
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+      setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
+    const id = open ? "simple-popover" : undefined;
+
+    return (
+      <div>
+        <Button aria-describedby={id} onClick={handleClick}>
+          Open Popover
+        </Button>
+        <PopOver
+          id={id}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+          transformOrigin={{ vertical: -10, horizontal: 0 }}
+        >
+          <Text>The content of the pop over.</Text>
+        </PopOver>
+      </div>
+    );
+  },
+  // play: async ({ canvasElement }) => {
+  //   const canvas = within(canvasElement);
+
+  //   await userEvent.type(canvas.getByRole("searchbox"), "River Nile");
+  //   await userEvent.click(canvas.getByRole("button", { name: "search" }));
+  // },
+};
