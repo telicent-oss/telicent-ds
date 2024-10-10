314f6bad4870f88d10bcc73a16e8d3d4b4acc4f8
dptelicent <dan.pearce@telicent.io>
2024-10-10 15:53:27 +0200

    refactor css

--- src/v1/components/data-display/UserProfile/UserProfile.tsx
+++ src/v1/components/data-display/UserProfile/UserProfile.tsx
@@ -3,7 +3,6 @@ import MUIStack from "@mui/material/Stack";
 import MUIIconButton from "@mui/material/IconButton";
 import MUIAvatar from "@mui/material/Avatar";
 import MUIMenu from "@mui/material/Menu";
-import { css } from "@emotion/react";
 
 import { H2 } from "../Text/Text";
 import UserIcon from "../Icons/UserIcon";
@@ -25,23 +24,16 @@ const UserProfile: React.FC<UserProfileProps> = ({ fullName, children }) => {
     setAnchorEl(null);
   };
 
-  const usernameStyle = css`
-    display: none;
-    @media (min-width: 768px) {
-      display: block;
-      max-width: 11rem;
-    }
-    @media (min-width: 1024px) {
-      max-width: 18rem;
-    }
-  `;
-
   return (
     <MUIStack direction="row" spacing={1} alignItems="center">
       <H2
         variant="h6"
-        css={usernameStyle}
         sx={{
+          display: { xs: "none", sm: "block" },
+          maxWidth: {
+            sm: "11rem",
+            md: "18rem",
+          },
           overflow: "hidden",
           whiteSpace: "nowrap",
           textOverflow: "ellipsis",
