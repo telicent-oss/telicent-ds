e53e677a56dd61c9cc676752ba3e4920c8e56552
dptelicent <dptelicent@users.noreply.github.com>
2024-10-15 17:44:20 +0000

    deploy: 226f87ac5e52bf8d64f71c0465a30303bef5d461

--- TELFE-222-Search-View-user-profile/src/providers/TeliThemeProvider/TeliThemeProvider.d.ts
+++ TELFE-222-Search-View-user-profile/src/providers/TeliThemeProvider/TeliThemeProvider.d.ts
@@ -4,7 +4,7 @@ type TeliThemeProviderProps = {
     children?: React.ReactNode;
 };
 /**
- * The ThemeProvider component is responsible for providing the theme to all it's children.
+ * The ThemeProvider component is responsible for providing the theme to all its children.
  * This component should be imported in index.jsx|tsx, if the standard layout component is not being used.
  */
 declare const TeliThemeProvider: React.FC<TeliThemeProviderProps>;
