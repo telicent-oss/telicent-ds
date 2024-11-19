1cae72cac02d3c85aacd85eedb6b17f07933066d
AlecsMissangu <AlecsMissangu@users.noreply.github.com>
2024-11-19 16:01:28 +0000

    deploy: d4e825572ef16120ed2d71db86df4ed372d2486e

---
+++ extend-teli-select-props/src/components/TeliButton/stories/SecondaryButtons.d.ts
@@ -0,0 +1,4 @@
+import { Story } from '../TeliButton.stories';
+
+declare const SecondaryButtons: Story;
+export default SecondaryButtons;
---
+++ extend-teli-select-props/src/v1/components/surfaces/index.d.ts
@@ -0,0 +1,6 @@
+export * from './Card';
+export * from './FloatingPanel';
+export { default as AppBar } from './AppBar/AppBar';
+export type { AppBarProps } from './AppBar/AppBar';
+export { default as PopOver } from './PopOver/Popover';
+export { default as Toolbar } from './Toolbar/Toolbar';
