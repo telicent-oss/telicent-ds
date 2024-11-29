55e5726eea4de21d7b1e066d3daeb9bb9c5a8c11
ACoolmanTelicent <ACoolmanTelicent@users.noreply.github.com>
2024-11-29 16:54:44 +0000

    deploy: d2561f491c6c5d67aa51cee89b217a213cc53532

---
+++ TELFE-563/test-rdf-libraries/src/components/TeliTable/stories/table-data.d.ts
@@ -0,0 +1,4 @@
+export declare const TV_SERIES_RANKINGS_COLUMNS: Array<string>;
+export declare const TV_SERIES_RANKINGS_ROWS: Array<Array<string>>;
+declare const TV_SERIES_RANKINGS: string[][];
+export default TV_SERIES_RANKINGS;
