d2561f491c6c5d67aa51cee89b217a213cc53532
ACoolmanTelicent <a.coleman@telicent.io>
2024-11-29 16:52:04 +0000

    fix(useOntologyStyles/DSProviders): remove

--- src/contexts/context-utils.test.ts
+++
@@ -1,8 +0,0 @@
-import { getOntologyClass } from "./context-utils";
-
-describe("context utils - getOntologyClass()", () => {
-  test("returns DoctorAppointment when uri is 'http://telicent.io/ontology/DoctorAppointment'", () => {
-    const fragment = getOntologyClass("http://telicent.io/ontology/DoctorAppointment");
-    expect(fragment).toBe("DoctorAppointment");
-  });
-});
