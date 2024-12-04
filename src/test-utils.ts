40dec0829dd1ea3c34926d1bff8e6cc9d1c5c03b
ACoolmanTelicent <a.coleman@telicent.io>
2024-12-04 14:46:15 +0000

    fix: typo

--- src/test-utils.ts
+++ src/test-utils.ts
@@ -1,6 +1,6 @@
 import { render, RenderOptions } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-{}
+
 export const setup = (jsx: JSX.Element, options: RenderOptions = {}) => ({
   user: userEvent.setup(),
   ...render(jsx, options),
