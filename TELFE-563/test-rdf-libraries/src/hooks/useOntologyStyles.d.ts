783e7c7c0e87f757be9fe942a023b0e3ed6ea0a5
ACoolmanTelicent <ACoolmanTelicent@users.noreply.github.com>
2024-12-13 15:06:18 +0000

    deploy: 5a4567132de24b3a729eb079fb30a4011ad02901

--- TELFE-563/test-rdf-libraries/src/hooks/useOntologyStyles.d.ts
+++ TELFE-563/test-rdf-libraries/src/hooks/useOntologyStyles.d.ts
@@ -1,5 +1,14 @@
 export declare function useOntologyStyles(): {
     isLoading: boolean;
-    styles: IconStyleType[] | null;
+    styles: {
+        backgroundColor: string;
+        color: string;
+        classUri: string;
+        iconFallbackText: string;
+        alt: string;
+        faIcon?: string | undefined;
+        faUnicode?: string | undefined;
+        shape?: string | undefined;
+    }[] | null;
     findIcon: undefined;
 };
