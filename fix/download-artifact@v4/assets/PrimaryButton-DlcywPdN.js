8f13f848c94cc1f41a8bf83540b0d7aad61947b1
ACoolmanTelicent <ACoolmanTelicent@users.noreply.github.com>
2025-01-16 15:47:25 +0000

    deploy: 9bf65dbc42d58e19ac784898c09cf894bf240c7e

---
+++ fix/download-artifact@v4/assets/PrimaryButton-DlcywPdN.js
@@ -0,0 +1,1 @@
+import{j as o}from"./DefaultPropsProvider--lJPAVOf.js";import{B as i}from"./Button-D07OoOtO.js";const t=r=>o(i,{variant:"contained",color:"primary",...r,children:r.children});t.__docgenInfo={description:"",methods:[],displayName:"PrimaryButton"};export{t as P};
