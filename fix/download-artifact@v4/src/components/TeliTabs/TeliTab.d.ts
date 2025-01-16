b775ba59ac31ce72b461cee59c365c79bd13d441
ACoolmanTelicent <ACoolmanTelicent@users.noreply.github.com>
2025-01-16 15:53:16 +0000

    deploy: 67379dd3c71adc4d9d8cfb02c8879e480301ae87

--- fix/download-artifact@v4/src/components/TeliTabs/TeliTab.d.ts
+++
@@ -1,8 +0,0 @@
-import { default as React } from '../../../node_modules/react';
-import { TabProps } from '@mui/material';
-
-interface TeliTabProps extends Omit<TabProps, "value"> {
-    tabIndex: number;
-}
-declare const TeliTab: React.FC<TeliTabProps>;
-export default TeliTab;
--- fix/download-artifact@v4/src/v1/theme/light-palette.d.ts
+++
@@ -1,5 +0,0 @@
-import { ThemeOptions } from '@mui/material';
-import { UITheme } from './colors/theme-colors';
-
-declare const createLightPalette: (themeColor: UITheme) => ThemeOptions["palette"];
-export default createLightPalette;
