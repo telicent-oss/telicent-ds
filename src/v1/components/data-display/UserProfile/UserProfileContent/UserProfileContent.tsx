1b379149b79ddfac6768180b049e4510b1050a36
Vanessa Kemp <vanessa.kemp@telicent.io>
2025-02-05 13:25:16 +0000

    remove commented out code

--- src/v1/components/data-display/UserProfile/UserProfileContent/UserProfileContent.tsx
+++ src/v1/components/data-display/UserProfile/UserProfileContent/UserProfileContent.tsx
@@ -1,20 +1,6 @@
 import React, { PropsWithChildren } from "react";
-import MUIAvatar from "@mui/material/Avatar";
 import MUIStack from "@mui/material/Stack";
 
-import { H2 } from "../../Text/Text";
-import UserIcon from "../../Icons/UserIcon";
-
-const UserProfileContent: React.FC<PropsWithChildren> = ({ children }) => (
-  <MUIStack rowGap={2}>
-    {/* <MUIStack direction="row" columnGap={1} alignItems="center">
-      <MUIAvatar variant="circular-outlined" sx={{ width: 32, height: 32 }}>
-        <UserIcon color="primary" />
-      </MUIAvatar>
-      <H2 variant="h4">User Profile</H2>
-    </MUIStack> */}
-    {children}
-  </MUIStack>
-);
+const UserProfileContent: React.FC<PropsWithChildren> = ({ children }) => <MUIStack rowGap={2}>{children}</MUIStack>;
 
 export default UserProfileContent;
