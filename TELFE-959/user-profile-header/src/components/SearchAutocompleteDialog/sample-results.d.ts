16c08a18879a499d767a84941f335ad9e80ba50b
VeeKemp <VeeKemp@users.noreply.github.com>
2025-02-05 13:40:16 +0000

    deploy: 5b07cf4c382bf8fc8327caf00aa2d8e34fe3dc64

--- TELFE-959/user-profile-header/src/components/SearchAutocompleteDialog/sample-results.d.ts
+++
@@ -1,6 +0,0 @@
-declare const RESULTS: {
-    id: string;
-    types: string[];
-    label: string;
-}[];
-export default RESULTS;
--- assets/createSvgIcon-BfNnQ0Hm.js
+++
@@ -1,1 +0,0 @@
-import{b as i}from"./DefaultPropsProvider-D55Wj1Sq.js";import{r as o}from"./index-Du_IP5cC.js";import{j as s}from"./jsx-runtime-Dwss98Nb.js";import{S as t}from"./SvgIcon-sXCG3KbY.js";function x(e,m){function r(n,a){return s.jsx(t,i({"data-testid":`${m}Icon`,ref:a},n,{children:e}))}return r.muiName=t.muiName,o.memo(o.forwardRef(r))}export{x as c};
