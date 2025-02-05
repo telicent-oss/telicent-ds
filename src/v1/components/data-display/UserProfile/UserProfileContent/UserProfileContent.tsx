e2d8b4713f5a58a4c2e41d5042a9c311875bc93e
Vanessa Kemp <vanessa.kemp@telicent.io>
2025-02-05 10:27:29 +0000

    fix: remove unused header on the user profile content to match current design

--- src/v1/components/data-display/UserProfile/UserProfileContent/UserProfileContent.tsx
+++ src/v1/components/data-display/UserProfile/UserProfileContent/UserProfileContent.tsx
@@ -7,12 +7,12 @@ import UserIcon from "../../Icons/UserIcon";
 
 const UserProfileContent: React.FC<PropsWithChildren> = ({ children }) => (
   <MUIStack rowGap={2}>
-    <MUIStack direction="row" columnGap={1} alignItems="center">
+    {/* <MUIStack direction="row" columnGap={1} alignItems="center">
       <MUIAvatar variant="circular-outlined" sx={{ width: 32, height: 32 }}>
         <UserIcon color="primary" />
       </MUIAvatar>
       <H2 variant="h4">User Profile</H2>
-    </MUIStack>
+    </MUIStack> */}
     {children}
   </MUIStack>
 );
