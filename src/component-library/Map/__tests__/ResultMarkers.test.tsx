779fac69f5f1e7cda8b567aeec300eaadcf3143a
Dave Cockrell <dave@telicent.io>
2025-02-24 11:19:11 +0000

    wip: work on tests

--- src/component-library/Map/__tests__/ResultMarkers.test.tsx
+++ src/component-library/Map/__tests__/ResultMarkers.test.tsx
@@ -1,40 +1,19 @@
 import React from "react";
 import { screen, waitFor } from "@testing-library/dom";
-import Map, { MapProvider } from "react-map-gl/maplibre";
+import Map from "react-map-gl/maplibre";
 
-import { renderWithProviders } from "../../../testUtils";
-import { DOCUMENT_LOCATIONS } from "../../../sampleData";
+import { } from "../../../test-utils"
 import ResultsMarkers from "../ResultsMarkers";
-import config from "../../../config/app-config";
-import { act } from "react-dom/test-utils";
-const CSS = {
-  MATCH_ICON_STYLE:
-    "background-color: rgb(36, 36, 0); color: rgb(255, 253, 4); display: flex; align-items: center; justify-content: center; border-color: #fffd04; border-radius: 9999px; cursor: pointer; border-width: 1px; width: 28px; height: 28px;",
-  NO_ICON_STYLE:
-    "background-color: rgb(18, 18, 18); color: rgb(221, 221, 221); display: flex; align-items: center; justify-content: center; border-color: #dddddd; border-radius: 9999px; cursor: pointer; border-width: 1px; width: 28px; height: 28px;",
-};
-const renderMarkers = () =>
-  renderWithProviders(
-    <Map>
-      <ResultsMarkers setViewportToBounds={jest.fn()} />
-    </Map>,
-    {
-      locationProviderProps: {
-        documentLocations: DOCUMENT_LOCATIONS,
-      },
-      wrapper: MapProvider,
-    }
-  );
 
 describe("Result Markers component", () => {
-  window.URL.createObjectURL = jest.fn();
+  // window.URL.createObjectURL = jest.fn();
 
   describe("renders correctly", () => {
     let container: HTMLElement;
     let qs: typeof container.querySelector;
     beforeEach(async () => {
       // beforeAll has side effects that cause N+1 test cases to fail
-      ({ container } = renderMarkers());
+      const { container } = render(<Map><ResultsMarkers setViewportToBounds={jest.fn()} /></Map>)
       qs = container.querySelector.bind(container);
       await waitFor(
         () => expect(screen.queryByTestId("document-locations")).toBeTruthy() // wait for use(promise)
