902c388f010892ffd98ca9ee2294f85f935fd5f9
VeeKemp <VeeKemp@users.noreply.github.com>
2025-02-25 13:44:54 +0000

    deploy: 23e85233b8de4df6c998fd1aab724dabb3bc9e7c

--- fix-appSwitch-on-AppBar/src/v1/theme/style-overrides/component-overrides/avatar-overrides.d.ts
+++
@@ -1,9 +0,0 @@
-import { ThemeOptions } from '@mui/material';
-
-declare module "@mui/material/Avatar" {
-    interface AvatarPropsVariantOverrides {
-        "circular-outlined": true;
-    }
-}
-declare const AVATAR_OVERRIDES: ThemeOptions["components"];
-export default AVATAR_OVERRIDES;
--- src/v1/components/surfaces/AppBar/AppBar.d.ts
+++ src/v1/components/surfaces/AppBar/AppBar.d.ts
@@ -1,11 +1,10 @@
 import { default as React } from '../../../../../node_modules/react';
 import { AppBarProps as MUIAppBarProps } from '@mui/material/AppBar';
-import { AppSwitchLibraryType } from '../../data-display/AppSwitch/AppSwitch';
 
 export type AppBarProps = Partial<{
     appName: string;
     beta: boolean;
-    apps: AppSwitchLibraryType;
+    startChild: React.ReactNode;
     endChild: React.ReactNode;
     position: MUIAppBarProps["position"];
     version: string;
