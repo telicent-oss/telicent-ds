a1a961d4a3b393659cbb6084beb7aa9852806fe2
VeeKemp <VeeKemp@users.noreply.github.com>
2025-02-25 08:52:17 +0000

    deploy: aaa997e379f39c404e0bfe3c8894cc838b62c785

---
+++ fix-appBar-dark-colour/src/v1/components/data-display/UserProfile/UserProfileContent/UserProfileContent.d.ts
@@ -0,0 +1,4 @@
+import { default as React, PropsWithChildren } from '../../../../../../node_modules/react';
+
+declare const UserProfileContent: React.FC<PropsWithChildren>;
+export default UserProfileContent;
