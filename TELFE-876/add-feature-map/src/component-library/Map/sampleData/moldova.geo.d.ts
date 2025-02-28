17804245b089cee796ba62debfdcb545c47142ab
davec504 <davec504@users.noreply.github.com>
2025-02-28 15:14:33 +0000

    deploy: a2a56ee712518190a36f4d7ffcd51261bab317c8

--- TELFE-876/add-feature-map/src/component-library/Map/sampleData/moldova.geo.d.ts
+++ TELFE-876/add-feature-map/src/component-library/Map/sampleData/moldova.geo.d.ts
@@ -1,11 +1,3 @@
-export declare const MOLDOVA: {
-    type: string;
-    properties: {
-        country: string;
-        iso3166_a3: string;
-    };
-    geometry: {
-        type: string;
-        coordinates: number[][][];
-    };
-};
+import { Feature } from 'geojson';
+
+export declare const MOLDOVA: Feature;
