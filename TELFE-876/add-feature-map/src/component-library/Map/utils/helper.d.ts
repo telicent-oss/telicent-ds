53daca6c7e2c12790537c15453e397deae7e306f
davec504 <davec504@users.noreply.github.com>
2025-03-03 10:17:00 +0000

    deploy: c259aee5a233a91c85b7f6dd3e7bf1e8466a90f8

--- TELFE-876/add-feature-map/src/component-library/Map/utils/helper.d.ts
+++ TELFE-876/add-feature-map/src/component-library/Map/utils/helper.d.ts
@@ -3,7 +3,7 @@ import { ResultMarker } from '../ResultsMarkers';
 
 export declare const getIconLabel: (uri: string) => string;
 export declare const getLabelCharacters: (label: string) => string;
-export declare const checkIsValidPosition: (arr: any[]) => boolean;
+export declare const checkIsValidPosition: (arr: unknown[]) => boolean;
 export declare const recursiveFlatten: (array: any[]) => any;
 export declare const getCoordinates: (geometry: Geometry) => any;
 export declare const calculateBounds: (markers?: ResultMarker[], polygons?: Feature[]) => import('maplibre-gl').LngLatBounds;
