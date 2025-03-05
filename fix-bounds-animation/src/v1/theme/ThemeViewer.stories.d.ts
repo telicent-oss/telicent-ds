9e21f27bfde535a549782adfcca9f46a0e6f1309
davec504 <davec504@users.noreply.github.com>
2025-03-05 17:26:29 +0000

    deploy: 5f0fac40e9fd920c22f0907b529f3066942c5fa2

--- fix-bounds-animation/src/v1/theme/ThemeViewer.stories.d.ts
+++
@@ -1,7 +0,0 @@
-import { Meta, StoryObj } from '@storybook/react';
-import { default as ThemeViewer } from './ThemeViewer';
-
-declare const meta: Meta<typeof ThemeViewer>;
-export default meta;
-type Story = StoryObj<typeof ThemeViewer>;
-export declare const Default: Story;
