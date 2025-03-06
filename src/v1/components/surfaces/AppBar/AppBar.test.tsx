0957622226f6b6e2c125d788de0cb8ec77af8a75
Vanessa Kemp <vanessa.kemp@telicent.io>
2025-03-06 10:18:55 +0000

    fix: update tests

--- src/v1/components/surfaces/AppBar/AppBar.test.tsx
+++ src/v1/components/surfaces/AppBar/AppBar.test.tsx
@@ -6,34 +6,27 @@ import { screen, within } from "@testing-library/dom";
 describe("AppBar Component", () => {
 
     test("should render the appName", () => {
-        setup(<AppBar appName="AppBar Test" />);
-  
-      expect(screen.getByText("AppBar Test")).toBeInTheDocument();
+      setup(<AppBar appName="AppBar Test" />);
+
+      expect(screen.getByText("APPBAR TEST")).toBeInTheDocument();
     });
 
     test("should render the startChild component", () => {
-       setup(
-        <AppBar appName="Design System" startChild={<div id="start-child">Start Child</div>} />
-      );
-  
-  
+      setup(<AppBar appName="Design System" startChild={<div id="start-child">Start Child</div>} />);
+
       expect(screen.getByTestId("start-child")).toBeInTheDocument();
       expect(screen.getByText("Start Child")).toBeInTheDocument();
     });
 
     test("should render the endChild component", () => {
-       setup(
-        <AppBar appName="Design System" endChild={<div id="end-child">End Child</div>} />
-      );
-  
+      setup(<AppBar appName="Design System" endChild={<div id="end-child">End Child</div>} />);
 
       expect(screen.getByTestId("end-child")).toBeInTheDocument();
       expect(screen.getByText("End Child")).toBeInTheDocument();
     });
-  
 
     test("should render both startChild and endChild components", () => {
-   setup(
+      setup(
         <AppBar
           appName="Design System"
           startChild={<div id="start-child">Start Child</div>}
@@ -43,22 +36,22 @@ describe("AppBar Component", () => {
 
       expect(screen.getByTestId("start-child")).toBeInTheDocument();
       expect(screen.getByText("Start Child")).toBeInTheDocument();
-  
+
       expect(screen.getByTestId("end-child")).toBeInTheDocument();
       expect(screen.getByText("End Child")).toBeInTheDocument();
     });
 
-    test("shoult render the version number", ()=>{
-       setup(<AppBar version="1.2.3"/>)
+    test("shoult render the version number", () => {
+      setup(<AppBar version="1.2.3" />);
 
-       expect(screen.getByText("1.2.3")).toBeInTheDocument();
-    })
+      expect(screen.getByText("1.2.3")).toBeInTheDocument();
+    });
 
     test("on click function", async () => {
       const handleClick = jest.fn();
       const { user } = setup(<AppBar appName="Test App" onClick={handleClick} />);
 
-      const appBar = screen.getByText("Test App");
+      const appBar = screen.getByText("TEST APP");
 
       await user.click(appBar);
 
