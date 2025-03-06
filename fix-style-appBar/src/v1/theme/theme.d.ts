05397be4b4fe041cc0289f7464c245559a96d849
VeeKemp <VeeKemp@users.noreply.github.com>
2025-03-06 10:24:29 +0000

    deploy: 1a21bef5ad377819ecdc965b62b9832ba783fbd5

--- fix-style-appBar/src/v1/theme/theme.d.ts
+++
@@ -1,6 +0,0 @@
-import { UITheme } from './colors/theme-colors';
-import { default as generateComponentOverrides } from './style-overrides/components';
-
-export type ComponentOverrides = ReturnType<typeof generateComponentOverrides>;
-declare const createTheme: (themeColor: UITheme, dark: boolean) => import('@mui/material/styles').Theme;
-export default createTheme;
