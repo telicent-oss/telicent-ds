d4db9400d1a7f90e78a7d074634959934e04534b
VeeKemp <VeeKemp@users.noreply.github.com>
2025-03-10 12:48:13 +0000

    deploy: 168b7e1967c0540c70ab2ca6378352d9ea03a6bb

---
+++ fix-appBar/src/v1/components/surfaces/Card/CardActions.d.ts
@@ -0,0 +1,4 @@
+import { default as React, PropsWithChildren } from '../../../../../node_modules/react';
+
+declare const CardActions: React.FC<PropsWithChildren>;
+export default CardActions;
---
+++ fix-appBar/src/v1/theme/dark-palette.d.ts
@@ -0,0 +1,5 @@
+import { ThemeOptions } from '@mui/material';
+import { UITheme } from './colors/theme-colors';
+
+declare const createDarkPalette: (themeColor: UITheme) => ThemeOptions["palette"];
+export default createDarkPalette;
