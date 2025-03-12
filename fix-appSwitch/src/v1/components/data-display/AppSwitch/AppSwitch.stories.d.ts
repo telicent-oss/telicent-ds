ec5e5b2972419133b7e65de3b507d6c3318df0d0
VeeKemp <VeeKemp@users.noreply.github.com>
2025-03-12 13:15:01 +0000

    deploy: 78ae162a699161fb6694449a8c9f91fbc8011b04

---
+++ fix-appSwitch/src/v1/components/data-display/AppSwitch/AppSwitch.stories.d.ts
@@ -0,0 +1,7 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { default as AppSwitch } from './AppSwitch';
+
+declare const meta: Meta<typeof AppSwitch>;
+export default meta;
+type Story = StoryObj<typeof AppSwitch>;
+export declare const AppSwitchExample: Story;
