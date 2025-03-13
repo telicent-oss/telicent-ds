4da2530fedc5d018ce004b01dc04e559854622d4
VeeKemp <VeeKemp@users.noreply.github.com>
2025-03-13 12:55:12 +0000

    deploy: 221a5895fca70536a0a5e845290ed06c4fbe025f

--- fix-update-bgcolor/src/v1/components/data-display/AppSwitch/AppSwitch.stories.d.ts
+++
@@ -1,7 +0,0 @@
-import { Meta, StoryObj } from '@storybook/react';
-import { default as AppSwitch } from './AppSwitch';
-
-declare const meta: Meta<typeof AppSwitch>;
-export default meta;
-type Story = StoryObj<typeof AppSwitch>;
-export declare const AppSwitchExample: Story;
