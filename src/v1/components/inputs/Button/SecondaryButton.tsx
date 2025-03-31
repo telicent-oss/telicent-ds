46fb2e5e61292ca79ca13623bcfdb57bd90c429b
davec504 <88431810+davec504@users.noreply.github.com>
2025-03-31 16:38:46 +0100

    fix: add refs to all buttons (#242)

--- src/v1/components/inputs/Button/SecondaryButton.tsx
+++ src/v1/components/inputs/Button/SecondaryButton.tsx
@@ -1,21 +1,21 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import MUIButton, { ButtonProps as MUIButtonProps } from "@mui/material/Button";
 
 type SecondaryButtonProps = Omit<MUIButtonProps, "variant" | "color">;
 
-const SecondaryButton: React.FC<SecondaryButtonProps> = (buttonProps) => {
+const SecondaryButton = forwardRef<HTMLButtonElement, SecondaryButtonProps>((buttonProps, ref) => {
   if (buttonProps.disableElevation) {
     return (
-      <MUIButton variant="outlined" color="primary" {...buttonProps}>
+      <MUIButton variant="outlined" color="primary" {...buttonProps} ref={ref}>
         {buttonProps.children}
       </MUIButton>
     );
   }
   return (
-    <MUIButton variant="outlined" color="inherit" {...buttonProps}>
+    <MUIButton variant="outlined" color="inherit" {...buttonProps} ref={ref}>
       {buttonProps.children}
     </MUIButton>
   );
-};
+});
 
 export default SecondaryButton;
