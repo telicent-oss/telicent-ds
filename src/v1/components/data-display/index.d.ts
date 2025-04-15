a3d23ee244ed3dd0aebd5388ffb83057ecfcad77
davec504 <davec504@users.noreply.github.com>
2025-04-15 12:20:16 +0000

    deploy: 8c3a4f08fdb75e4224c57af6dbf8580592d1b409

--- src/v1/components/data-display/index.d.ts
+++ src/v1/components/data-display/index.d.ts
@@ -18,3 +18,4 @@ export { default as UserProfileContent } from './UserProfile/UserProfileContent/
 export { default as TitleAndContent } from './Text/TitleAndContent/TitleAndContent';
 export { default as UserStatus } from './UserProfile/UserProfileContent/UserStatus';
 export type { UserStatusProps } from './UserProfile/UserProfileContent/UserStatus';
+export { default as DropdownButton } from './DropdownButton/DropdownButton';
