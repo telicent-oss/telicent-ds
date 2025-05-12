2f8a68625b1fb1ec7de59626f8aa097666e908e1
ACoolmanTelicent <ACoolmanTelicent@users.noreply.github.com>
2025-05-12 08:24:41 +0000

    deploy: cef7d28d07f4ad6752bde37dc2f539b33f14ad04

---
+++ TELFE-1128/graph-query-param/src/components/TeliAvatar/TeliUserAvatar.d.ts
@@ -0,0 +1,23 @@
+import { default as React } from '../../../node_modules/react';
+import { AvatarProps } from '@mui/material/Avatar';
+
+export interface TeliUserAvatarProps {
+    /**
+     * Provides an alt attribute for the rendered img element.
+     */
+    alt: AvatarProps["alt"];
+    /**
+     * User's first name
+     */
+    firstName: string;
+    /**
+     * User's last name
+     */
+    lastName: string;
+    /**
+     * The src attribute for the img element.
+     */
+    src: AvatarProps["src"];
+}
+declare const TeliUserAvatar: React.FC<Partial<TeliUserAvatarProps>>;
+export default TeliUserAvatar;
---
+++ TELFE-1128/graph-query-param/src/v1/theme/colors/admin-blue.d.ts
@@ -0,0 +1,9 @@
+declare const ADMIN_BLUE: {
+    50: string;
+    100: string;
+    200: string;
+    400: string;
+    500: string;
+    600: string;
+};
+export default ADMIN_BLUE;
