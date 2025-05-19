d32113363aa4ad00c0fbdd8167c4f23a09fec508
Vanessa Kemp <vanessa.kemp@telicent.io>
2025-05-19 13:49:41 +0100

    fix: update react schema to fix modals

--- src/schema/reactElementsSchema.ts
+++ src/schema/reactElementsSchema.ts
@@ -1,8 +1,10 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 // Simulating React.ReactElement validation
 export const reactElementSchema = z.object({
-    type: z.union([z.string(), z.function(), z.symbol()]), // Type can be a string or a function (JSXElementConstructor)
-    props: z.record(z.any()),                 // Props is an object with any keys
-    key: z.union([z.string(), z.number(), z.null()]).optional(), // Key is either string, number, or null
+  type: z.union([z.string(), z.function(), z.symbol(), z.object({}).passthrough()]), // Type can be a string or a function (JSXElementConstructor)
+  props: z.record(z.any()), // Props is an object with any keys
+  key: z.union([z.string(), z.number(), z.null()]).optional(), // Key is either string, number, or null
 });
+
+// z.object({}).passthrough() ← allow component types that are objects (e.g. styled, memo, forwardRef
