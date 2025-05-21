2057498da775444f0426c9a1584455dba508b175
ACoolmanTelicent <a.coleman@telicent.io>
2025-05-21 14:59:57 +0100

    fix(logout-syncer): [TELFE-1131] requestWipe blocks normal logout

--- src/candidate-packages/logout-syncer/requestWipe.ts
+++ src/candidate-packages/logout-syncer/requestWipe.ts
@@ -1,5 +1,8 @@
+import { autoLoggedOut } from "./utils/autoLoggedOut";
+
 export const requestWipe = () => {
   if (navigator.serviceWorker?.controller) {
+    autoLoggedOut.set(Date.now()); // as this page is signing out - block auto triggerWipe
     navigator.serviceWorker.controller.postMessage({ type: 'sign-out-request' });
   } else {
     console.error('Expected navigator.serviceWorker.controller, instead undefined; Thus cannot run requestWipe()')
