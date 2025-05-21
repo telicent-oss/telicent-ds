e24bbdfd36351ca441a67ca10673c757cebbae01
ACoolmanTelicent <ACoolmanTelicent@users.noreply.github.com>
2025-05-21 13:06:20 +0000

    deploy: 370d0a5549865a4a0a26c356971115723c9ba6b0

--- TELFE-1131/logout-syncer/src/candidate-packages/logout-syncer/utils/autoLoggedOut.d.ts
+++ TELFE-1131/logout-syncer/src/candidate-packages/logout-syncer/utils/autoLoggedOut.d.ts
@@ -1,11 +1,9 @@
-declare function get(): string | null;
+declare function get(): string | undefined;
 declare function remove(): void;
 declare function set(val: number): void;
 export declare const autoLoggedOut: {
-    sessionStorage: {
-        get: typeof get;
-        set: typeof set;
-        remove: typeof remove;
-    };
+    get: typeof get;
+    set: typeof set;
+    remove: typeof remove;
 };
 export {};
