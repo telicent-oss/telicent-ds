4a4655d898f9a5b998c5317d8c53759371a49b1f
ACoolmanTelicent <ACoolmanTelicent@users.noreply.github.com>
2025-05-30 16:15:11 +0000

    deploy: 467f6692fcf649bce4fca4239d900a074f7648a6

--- fix/theme-provider-cache/src/v1/theme/theme.d.ts
+++
@@ -1,9 +0,0 @@
-import { ThemeOptions } from '@mui/material/styles';
-import { UITheme } from './colors/theme-colors';
-import { default as generateComponentOverrides } from './style-overrides/components';
-
-export type ComponentOverrides = ReturnType<typeof generateComponentOverrides>;
-export declare const createTheme: (uiTheme: UITheme, palette: ThemeOptions["palette"]) => import('@mui/material/styles').Theme;
-export declare const createThemeCached: (uiTheme: UITheme, dark: boolean, skipCache?: boolean) => import('@mui/material/styles').Theme;
-export declare const EMPTY_THEME: UITheme;
-export default createThemeCached;
