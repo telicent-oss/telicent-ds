4fa8befb27723f992771242b80160f04c027d165
ACoolmanTelicent <ACoolmanTelicent@users.noreply.github.com>
2025-06-19 14:38:12 +0000

    deploy: dffce21800655851f0c0370236bea0e41216d7bc

--- TELFE-1096/auth/src/candidate-packages/authorize-flow/AuthModal.d.ts
+++ TELFE-1096/auth/src/candidate-packages/authorize-flow/AuthModal.d.ts
@@ -1,10 +1,7 @@
-import { UITheme } from '../../export';
-
 interface AuthRedirectModalProps {
     channelName?: string;
     signOutUrl: string;
     debounceMs?: number;
-    theme?: UITheme;
 }
 export declare const AuthModal: React.FC<AuthRedirectModalProps>;
 export {};
