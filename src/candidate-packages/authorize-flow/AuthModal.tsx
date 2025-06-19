dffce21800655851f0c0370236bea0e41216d7bc
ACoolmanTelicent <a.coleman@telicent.io>
2025-06-19 15:36:16 +0100

    fix: [TELFE-1096] syntax error

--- src/candidate-packages/authorize-flow/AuthModal.tsx
+++ src/candidate-packages/authorize-flow/AuthModal.tsx
@@ -4,64 +4,68 @@ import { FlexBox, Button, Modal } from "../../export";
 import { Box } from "@mui/material";
 
 interface AuthRedirectModalProps {
-	channelName?: string;
-	signOutUrl: string;
-	debounceMs?: number;
+  channelName?: string;
+  signOutUrl: string;
+  debounceMs?: number;
 }
 
 export const AuthModal: React.FC<AuthRedirectModalProps> = ({
-	channelName = "auth-events",
-	signOutUrl,
-	debounceMs = 5000,
+  channelName = "auth-events",
+  signOutUrl,
+  debounceMs = 5000,
 }) => {
-	const bc = new BroadcastChannel(channelName);
-	const alreadyTriggered = useRef(false);
-	const [isOpen, setIsOpen] = useState(false);
+  const bc = new BroadcastChannel(channelName);
+  const alreadyTriggered = useRef(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-	const handleLoginClick = () => {
-		window.open(signOutUrl, "_blank");
-	};
+  const handleLoginClick = () => {
+    window.open(signOutUrl, "_blank");
+  };
 
-	const handleAuthMessage = useCallback((message: MessageEvent) => {
-		if (message.data.toLowerCase() === "unauthorized" && !alreadyTriggered.current) {
-			setIsOpen(true);
-			alreadyTriggered.current = true;
-			setTimeout(() => (alreadyTriggered.current = false), debounceMs);
-		} else if (message.data.toLowerCase() === "authorized") {
-			setIsOpen(false);
-			alreadyTriggered.current = false;
-		}
-	}, []);
+  const handleAuthMessage = useCallback((message: MessageEvent) => {
+    if (
+      message.data.toLowerCase() === "unauthorized" &&
+      !alreadyTriggered.current
+    ) {
+      setIsOpen(true);
+      alreadyTriggered.current = true;
+      setTimeout(() => (alreadyTriggered.current = false), debounceMs);
+    } else if (message.data.toLowerCase() === "authorized") {
+      setIsOpen(false);
+      alreadyTriggered.current = false;
+    }
+  }, []);
 
-	useEffect(() => {
-		if (!channelName) return
-		bc.addEventListener("message", handleAuthMessage);
-		return () => {
-			bc.removeEventListener("message", handleAuthMessage);
-			bc.close();
-		}
-	}, [channelName]);
+  useEffect(() => {
+    if (!channelName) return;
+    bc.addEventListener("message", handleAuthMessage);
+    return () => {
+      bc.removeEventListener("message", handleAuthMessage);
+      bc.close();
+    };
+  }, [channelName]);
 
-	return (
-		<Modal hideCloseButton onClose={() => { }} sx={{ m: 2, p: 2 }} open={isOpen}>
-			<FlexBox sx={{ p: 2 }}>
-				<H3>
-					<i className="fa-regular fa-circle-exclamation"></i> Your session is no longer active
-				</H3>
-				<Text sx={{ pt: 4 }}>
-					Your session may have expired, you may have signed out, or your account isn&apos;t active.
-				</Text>
-Text>Please login again to continue.</Text>
-			<Text sx={{ pt: 4 }}>
-				If you continue to have issues, contact your system administrator.
-			</Text>
-			<Box ml="auto" mt={2}>
-				<Button variant="primary" onClick={handleLoginClick} size="large">
-					Login
-				</Button>
-			</Box>
-		</FlexBox>
-		</Modal >
-	);
+  return (
+    <Modal hideCloseButton onClose={() => {}} sx={{ m: 2, p: 2 }} open={isOpen}>
+      <FlexBox sx={{ p: 2 }}>
+        <H3>
+          <i className="fa-regular fa-circle-exclamation"></i> Your session is
+          no longer active
+        </H3>
+        <Text sx={{ pt: 4 }}>
+          Your session may have expired, you may have signed out, or your
+          account isn&apos;t active.
+        </Text>
+        <Text>Please login again to continue.</Text>
+        <Text sx={{ pt: 4 }}>
+          If you continue to have issues, contact your system administrator.
+        </Text>
+        <Box ml="auto" mt={2}>
+          <Button variant="primary" onClick={handleLoginClick} size="large">
+            Login
+          </Button>
+        </Box>
+      </FlexBox>
+    </Modal>
+  );
 };
-
