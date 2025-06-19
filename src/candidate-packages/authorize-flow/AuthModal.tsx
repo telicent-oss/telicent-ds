f361f153cf7b472744e853a8e3cd127f926ac4f0
Dave Cockrell <dave@telicent.io>
2025-06-19 13:33:54 +0100

    feat: add auth modal

---
+++ src/candidate-packages/authorize-flow/AuthModal.tsx
@@ -0,0 +1,76 @@
+import { useState, useRef, useCallback, useEffect } from "react";
+import { H3, Text } from "../../v1/components/data-display/Text/Text";
+import { FlexBox, UITheme, Button, Modal } from "../../export";
+import { Box } from "@mui/material";
+import UIThemeProvider from "../../v1/theme/UIThemeProvider";
+
+interface AuthRedirectModalProps {
+	channelName?: string;
+	signOutUrl: string;
+	debounceMs?: number;
+	theme?: UITheme;
+}
+
+export const AuthModal: React.FC<AuthRedirectModalProps> = ({
+	channelName = "auth-events",
+	signOutUrl,
+	debounceMs = 5000,
+	theme = "DocumentPink"
+}) => {
+	const bc = new BroadcastChannel(channelName);
+	const alreadyTriggered = useRef(false);
+	const [isOpen, setIsOpen] = useState(false);
+
+	const handleLoginClick = () => {
+		window.open(signOutUrl, "_blank");
+	};
+
+	const handleAuthMessage = useCallback((message: MessageEvent) => {
+		console.log({ message, triggered: alreadyTriggered.current, channelName })
+		if (message.data.toLowerCase() === "unauthorized" && !alreadyTriggered.current) {
+			setIsOpen(true);
+			alreadyTriggered.current = true;
+			setTimeout(() => (alreadyTriggered.current = false), debounceMs);
+		} else if (message.data.toLowerCase() === "authorized") {
+			setIsOpen(false);
+			alreadyTriggered.current = false;
+		}
+	}, []);
+
+	useEffect(() => {
+		if (!channelName) return
+		bc.addEventListener("message", handleAuthMessage);
+		return () => {
+			bc.removeEventListener("message", handleAuthMessage);
+			bc.close();
+		}
+	}, [channelName]);
+
+
+	if (!signOutUrl) return null;
+
+	return (
+		<UIThemeProvider dark theme={theme}>
+			<Modal hideCloseButton onClose={() => { }} sx={{ m: 2, p: 2 }} open={isOpen}>
+				<FlexBox sx={{ p: 2 }}>
+					<H3>
+						<i className="fa-regular fa-circle-exclamation"></i> Your session is no longer active
+					</H3>
+					<Text sx={{ pt: 4 }}>
+						Your session may have expired, you may have signed out, or your account isn&apos;t active.
+					</Text>
+					<Text>Please login again to continue.</Text>
+					<Text sx={{ pt: 4 }}>
+						If you continue to have issues, contact your system administrator.
+					</Text>
+					<Box ml="auto" mt={2}>
+						<Button variant="primary" onClick={handleLoginClick} size="large">
+							Login
+						</Button>
+					</Box>
+				</FlexBox>
+			</Modal>
+		</UIThemeProvider>
+	);
+};
+
---
+++ src/candidate-packages/authorize-flow/withSessionHandling.ts
@@ -0,0 +1,28 @@
+import { AxiosInstance } from "axios";
+import { SessionHandlingConfig } from "./types";
+
+export function withSessionHandling(
+  instance: AxiosInstance,
+  {
+    queryClient,
+    keysToInvalidate = [],
+    broadcastChannel,
+  }: SessionHandlingConfig
+) {
+  instance.interceptors.response.use(
+    (res) => res,
+    (err) => {
+      if (err.response?.status === 401) {
+        if (queryClient) {
+          keysToInvalidate.forEach((key) =>
+            queryClient.invalidateQueries({ queryKey: key, exact: true })
+          );
+        }
+        broadcastChannel?.postMessage("unauthorized");
+      }
+      return Promise.reject(err);
+    }
+  );
+
+  return { instance };
+}
