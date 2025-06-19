c797a00337569b5269e4dd12e70d4441c256a059
Dave Cockrell <dave@telicent.io>
2025-06-19 14:29:11 +0100

    fix: pr suggestions

--- src/candidate-packages/authorize-flow/AuthModal.tsx
+++ src/candidate-packages/authorize-flow/AuthModal.tsx
@@ -1,21 +1,18 @@
 import { useState, useRef, useCallback, useEffect } from "react";
 import { H3, Text } from "../../v1/components/data-display/Text/Text";
-import { FlexBox, UITheme, Button, Modal } from "../../export";
+import { FlexBox, Button, Modal } from "../../export";
 import { Box } from "@mui/material";
-import UIThemeProvider from "../../v1/theme/UIThemeProvider";
 
 interface AuthRedirectModalProps {
 	channelName?: string;
 	signOutUrl: string;
 	debounceMs?: number;
-	theme?: UITheme;
 }
 
 export const AuthModal: React.FC<AuthRedirectModalProps> = ({
 	channelName = "auth-events",
 	signOutUrl,
 	debounceMs = 5000,
-	theme = "DocumentPink"
 }) => {
 	const bc = new BroadcastChannel(channelName);
 	const alreadyTriggered = useRef(false);
@@ -26,7 +23,6 @@ export const AuthModal: React.FC<AuthRedirectModalProps> = ({
 	};
 
 	const handleAuthMessage = useCallback((message: MessageEvent) => {
-		console.log({ message, triggered: alreadyTriggered.current, channelName })
 		if (message.data.toLowerCase() === "unauthorized" && !alreadyTriggered.current) {
 			setIsOpen(true);
 			alreadyTriggered.current = true;
@@ -46,31 +42,26 @@ export const AuthModal: React.FC<AuthRedirectModalProps> = ({
 		}
 	}, [channelName]);
 
-
-	if (!signOutUrl) return null;
-
 	return (
-		<UIThemeProvider dark theme={theme}>
-			<Modal hideCloseButton onClose={() => { }} sx={{ m: 2, p: 2 }} open={isOpen}>
-				<FlexBox sx={{ p: 2 }}>
-					<H3>
-						<i className="fa-regular fa-circle-exclamation"></i> Your session is no longer active
-					</H3>
-					<Text sx={{ pt: 4 }}>
-						Your session may have expired, you may have signed out, or your account isn&apos;t active.
-					</Text>
-					<Text>Please login again to continue.</Text>
-					<Text sx={{ pt: 4 }}>
-						If you continue to have issues, contact your system administrator.
-					</Text>
-					<Box ml="auto" mt={2}>
-						<Button variant="primary" onClick={handleLoginClick} size="large">
-							Login
-						</Button>
-					</Box>
-				</FlexBox>
-			</Modal>
-		</UIThemeProvider>
+		<Modal hideCloseButton onClose={() => { }} sx={{ m: 2, p: 2 }} open={isOpen}>
+			<FlexBox sx={{ p: 2 }}>
+				<H3>
+					<i className="fa-regular fa-circle-exclamation"></i> Your session is no longer active
+				</H3>
+				<Text sx={{ pt: 4 }}>
+					Your session may have expired, you may have signed out, or your account isn&apos;t active.
+				</Text>
+Text>Please login again to continue.</Text>
+			<Text sx={{ pt: 4 }}>
+				If you continue to have issues, contact your system administrator.
+			</Text>
+			<Box ml="auto" mt={2}>
+				<Button variant="primary" onClick={handleLoginClick} size="large">
+					Login
+				</Button>
+			</Box>
+		</FlexBox>
+		</Modal >
 	);
 };
 
