7cdf19e3730a6cc5458bea247d3f6f0cd118a353
VeeKemp <VeeKemp@users.noreply.github.com>
2025-07-04 13:59:18 +0000

    deploy: c318b1e91e6609f1070d3b29b561d33573a9e265

--- TELFE-1227-checkbox/src/v1/theme/colors/palette/createDarkPalette.d.ts
+++
@@ -1,5 +0,0 @@
-import { ThemeOptions } from '@mui/material';
-import { UITheme } from '../theme-colors';
-
-declare const createDarkPalette: (uiTheme: UITheme) => ThemeOptions["palette"];
-export default createDarkPalette;
--- src/v1/components/inputs/index.d.ts
+++ src/v1/components/inputs/index.d.ts
@@ -5,4 +5,5 @@ export { default as TextField } from './TextField';
 export { default as Select } from './Select/Select';
 export type { SelectProps } from './Select/Select';
 export type { Options } from './Select/Select';
+export { default as Checkbox } from './Checkbox/Checkbox';
 export * from './SearchBox';
