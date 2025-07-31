fb8ef35818c12612adcf07fb5858a492469e430d
Vanessa Kemp <vanessa.kemp@telicent.io>
2025-07-31 08:56:29 +0100

    fix: addressing pr comments

--- src/v1/components/inputs/CopyToClipboard/CopyToClipboard.tsx
+++ src/v1/components/inputs/CopyToClipboard/CopyToClipboard.tsx
@@ -9,15 +9,15 @@ import { ButtonProps } from "@mui/material";
 export type CopyToClipboardProps = ButtonProps & { text: string; title: string; ariaLabel?: string };
 
 const CopyToClipboard: React.FC<CopyToClipboardProps> = ({ text, title, ariaLabel = "copy to clipboard", sx }) => {
-  const [copyIcon, setCopyIcon] = useState(faCopy);
+  const [icon, setIcon] = useState(faCopy);
   const theme = useExtendedTheme();
 
   const handleClick = async () => {
     try {
       await navigator.clipboard.writeText(text);
-      setCopyIcon(faCheck);
+      setIcon(faCheck);
       setTimeout(() => {
-        setCopyIcon(faCopy);
+        setIcon(faCopy);
       }, 1500);
     } catch (error) {
       console.error("Clipboard write failed", error);
@@ -33,7 +33,7 @@ const CopyToClipboard: React.FC<CopyToClipboardProps> = ({ text, title, ariaLabe
       aria-label={ariaLabel}
       sx={{ color: theme.palette.primary.main, ...sx }}
     >
-      <FontAwesomeIcon icon={copyIcon} />
+      <FontAwesomeIcon icon={icon} />
     </Button>
   );
 };
