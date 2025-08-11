ed5cf967010c6223bff045d9158578950de76449
VeeKemp <VeeKemp@users.noreply.github.com>
2025-08-11 11:31:54 +0000

    deploy: 032c6f6d9b80a138212ee504685fbfaf5e03b330

--- TELFE-1276-fix-buttons/assets/index-DC6QxbS8.js
+++
@@ -1,1 +0,0 @@
-import{n as a,o,p as l}from"./Drawer-q4Gg0w5t.js";import"./index-Dl6G-zuu.js";const t={Dock:o,DockItem:a,DraggablePanel:l};export{t as F};
---
+++ TELFE-1276-fix-buttons/assets/index-EIo4rq7T.js
@@ -0,0 +1,1 @@
+import{n as a,o,p as l}from"./Drawer-CLbxZv9v.js";import"./index-Dl6G-zuu.js";const t={Dock:o,DockItem:a,DraggablePanel:l};export{t as F};
