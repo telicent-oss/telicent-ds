784996c314ff889c8a416d277b80724f3acb6c6b
Vanessa Kemp <vanessa.kemp@telicent.io>
2025-08-11 12:10:18 +0100

    fix: update export

--- src/v1/components/buttons/index.ts
+++ src/v1/components/buttons/index.ts
@@ -2,8 +2,6 @@ export { default as Button } from "./Button/Button";
 
 export { default as IconButton } from "./Button/IconButton";
 
-export { default as CopyToClipboard } from "./CopyToClipboardOLD/CopyToClipboard";
-
 export type { ButtonProps } from "./Button/Button";
 
 export { default as Link } from "./Link/LinkButton";
