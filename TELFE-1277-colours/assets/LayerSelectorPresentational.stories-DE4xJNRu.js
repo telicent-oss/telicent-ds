89b99834776883a829a1d3277a25062ee12af96c
VeeKemp <VeeKemp@users.noreply.github.com>
2025-08-12 14:03:45 +0000

    deploy: 4f24bba488d7b2f5e04fc7b1a1acc43d42442459

---
+++ TELFE-1277-colours/assets/LayerSelectorPresentational.stories-DE4xJNRu.js
@@ -0,0 +1,30 @@
+import{a as p,j as o}from"./emotion-react-jsx-runtime.browser.esm-BgDa5HT4.js";import{b as a,c as n}from"./Drawer-rG9ZKUgv.js";import"./jsx-runtime-DMAvRu52.js";import"./index-Dl6G-zuu.js";import"./createSvgIcon-MUYTbX0p.js";import"./defaultTheme-DzLjz-dB.js";import"./SvgIcon-ByXbI411.js";import"./styled-gOU_XONt.js";import"./generateUtilityClasses-Btm8e7dB.js";import"./DefaultPropsProvider-CJK-usBy.js";import"./constants-Ds6WvxFK.js";import"./iframe-DMvd1uko.js";import"../sb-preview/runtime.js";import"./index-B0kOWIl9.js";import"./types-JQgdQoTs.js";import"./useTheme-BorXEO18.js";import"./useTheme-Bb3Mws8A.js";import"./GlobalStyles-BSLRMkoa.js";import"./GlobalStyles-B-ilzbNG.js";import"./index-Nuk3PsgH.js";import"./index-BfyspvgH.js";import"./ButtonBase-DExEX2yL.js";import"./TransitionGroupContext-BU5kpZ0L.js";import"./useEnhancedEffect-DZnfS5Iy.js";import"./index-BbVSaS1S.js";import"./Popover-DyPBubII.js";import"./utils-BgVWvv5k.js";import"./Paper-BG42kFkz.js";import"./TextField-Cugujejn.js";import"./useId-CkA8naEk.js";import"./Menu-BldO7eLG.js";import"./AdapterDayjs-BiMd0sr1.js";import"./useThemeProps-D7jdKLbb.js";import"./useThemeProps-BLCE57Tg.js";import"./Typography-Dhe6o_9I.js";import"./colorManipulator-BRx-aaGP.js";import"./IconButton-CV1Rw-GO.js";import"./createStyled-HgSNW-x4.js";import"./Button-B3igqjqf.js";import"./Chip-CpI9GRDB.js";import"./TelicentHorizontalSVG-Dp9fAf73.js";import"./DataSetIcon-BPiH7weE.js";import"./TelicentMark-BQOfTp7R.js";import"./FlexGrid-BoEfBcU9.js";import"./Container-FF4Fdp1b.js";import"./styled-BrfVIC6E.js";import"./FlexBox-DVTFmfFO.js";import"./Box-Dhn_9KCB.js";import"./UserIcon-DpDI8rF2.js";import"./UserProfile-C5BZOrXB.js";import"./Text-BVIdl1Jw.js";import"./DateTimePicker-CwfcIARZ.js";import"./Divider-rhjpeBHv.js";import"./UIThemeProvider-U9ma58Q9.js";import"./ThemeProvider-GiwL96Kb.js";import"./IconButton-6csnvzSG.js";import"./Chip-tBdBanDh.js";import"./Divider-CByba2Al.js";import"./TreeView-0G5LkfNF.js";import"./UserProfileContent-CQNj73V_.js";import"./LinearProgress-BvlhFhje.js";import"./DatePicker-D8i7pChj.js";import"./Button-2o7EfnUw.js";import"./LinkButton-udFJw26p.js";import"./Card-B74XhqI-.js";import"./CardActions-BsipVerF.js";import"./Popover-BwutmoMe.js";import"./Paper-BsJ-nzzS.js";import"./ErrorFallback-Db8jP4lY.js";import"./ErrorFallbackText-B2VVr9Qi.js";import"./ErrorFallbackWrapper-CwjhO7ym.js";import"./MapToggleButtonPresentational-Dfb5p_Zu.js";import"./List-D9PDgvVC.js";const s=[{uri:"u1",image:"https://dummyimage.com/25x20&text=1",label:"First"},{uri:"u2",image:"https://dummyimage.com/25x20&text=2",label:"Second"},{uri:"u3",image:"https://dummyimage.com/25x20&text=3",label:"Third"}],l=r=>p("div",{children:[o(a,{color:"secondary",variant:"outlined",...r}),o("style",{children:`
+          body {
+            overflow: auto !important;
+          }
+          #layer-selector-popover {
+            pointer-events: none !important;
+          }
+        `}),o(n,{...r})]}),Mt={title:"Component Library/Map/primitives/LayerSelectorPresentational",component:l,tags:["autodocs"],parameters:{docs:{description:{component:`
+Renders a fake Presentational LayerSelector to help demonstrate how the default LayerSelector is assembled (used in FeatureMap)
+
+
+<p>
+  👉 Open 
+  <a href="iframe.html?viewMode=docs&id=component-library-map-composites-featuremap--docs" target="_blank">
+    FeatureMap story
+  </a>
+</p>
+
+**NOTE** the PopOver story renders with position absolute, there is some hack styles to unlock the page when popover is visible.
+
+• \`data\`: Array of { uri, image, label }  
+• \`selectedIndex\` & \`anchorEl\`: control popover state  
+• Handlers: \`onClickDropdown\`, \`onCloseDropdown\`, \`onListItemClick\`
+`}}},argTypes:{data:{control:"object"},selectedIndex:{control:{type:"number",min:0,max:2}},anchorEl:{control:!1},onClickDropdown:{action:"dropdownClicked"},onCloseDropdown:{action:"dropdownClosed"},onListItemClick:{action:"itemClicked"}}},t={args:{data:s,selectedIndex:0,anchorEl:document.createElement("button")}};var e,i,m;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    data: sampleData,
+    selectedIndex: 0,
+    anchorEl: document.createElement("button")
+  }
+}`,...(m=(i=t.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};const Ot=["Closed"];export{t as Closed,Ot as __namedExportsOrder,Mt as default};
