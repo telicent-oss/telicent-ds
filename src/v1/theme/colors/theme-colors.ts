26d3905c0eb0806e632a86579d38fd1f29b01b8d
Vanessa Kemp <vanessa.kemp@telicent.io>
2025-08-13 15:21:20 +0100

    fix: blank theme name

--- src/v1/theme/colors/theme-colors.ts
+++ src/v1/theme/colors/theme-colors.ts
@@ -3,7 +3,7 @@ import DataNavy from "./DataNavy";
 import DocumentPink from "./DocumentPink";
 import GraphOrange from "./GraphOrange";
 import AdminBlue from "./AdminBlue";
-import Blank from "./blank";
+import Blank from "./BlankTheme";
 
 export const UIThemeSchema = zod.enum(["DataNavy", "DocumentPink", "GraphOrange", "AdminBlue", "Blank"]);
 export type UITheme = zod.infer<typeof UIThemeSchema>;
