b5389c1af4edf53be9a2a1be2eb8794ef39691e1
Vanessa Kemp <102584595+VeeKemp@users.noreply.github.com>
2025-08-14 15:19:26 +0100

    fix: export textField (#306)

--- src/v1/components/inputs/index.ts
+++ src/v1/components/inputs/index.ts
@@ -1,4 +1,4 @@
-// export { default as TextField } from "./TextField/TextField";
+export { default as TextField } from "./TextField/TextField";
 export { default as TooltipToggleButton } from "./TooltipToggleButton";
 
 export { default as Select } from "./Select/Select";
