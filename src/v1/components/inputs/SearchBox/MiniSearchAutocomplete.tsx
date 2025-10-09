b5fe766d8130de60f83a5021e8fb0dc532050e42
Vanessa Kemp <102584595+VeeKemp@users.noreply.github.com>
2025-10-09 14:20:20 +0100

    Update src/v1/components/inputs/SearchBox/MiniSearchAutocomplete.tsx
    Co-authored-by: davec504 <88431810+davec504@users.noreply.github.com>
    Signed-off-by: Vanessa Kemp <102584595+VeeKemp@users.noreply.github.com>

--- src/v1/components/inputs/SearchBox/MiniSearchAutocomplete.tsx
+++ src/v1/components/inputs/SearchBox/MiniSearchAutocomplete.tsx
@@ -127,7 +127,7 @@ const MiniSearchAutocomplete = forwardRef(function Autocomplete<
     ...autocompleteProps,
   });
 
-  const updateCancelButton = `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='${encodeURIComponent(
+  const cancelIcon = `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='${encodeURIComponent(
     theme.palette.primary.main
   )}'><path d='M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z'/></svg>") no-repeat center`;
 
