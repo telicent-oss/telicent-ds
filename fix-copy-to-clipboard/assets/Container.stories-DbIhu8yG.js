29b50c4a2d9a6448a020256b88936d9bd22fb18f
VeeKemp <VeeKemp@users.noreply.github.com>
2025-10-14 10:18:59 +0000

    deploy: 6a03253145d2b0253cae9827092823a6f75a88f8

---
+++ fix-copy-to-clipboard/assets/Container.stories-DbIhu8yG.js
@@ -0,0 +1,31 @@
+import{j as s}from"./emotion-react-jsx-runtime.browser.esm-BgDa5HT4.js";import{a as r}from"./Container-FF4Fdp1b.js";import"./jsx-runtime-DMAvRu52.js";import"./index-Dl6G-zuu.js";import"./styled-gOU_XONt.js";import"./defaultTheme-DzLjz-dB.js";import"./styled-BrfVIC6E.js";import"./createStyled-HgSNW-x4.js";import"./useThemeProps-BLCE57Tg.js";import"./DefaultPropsProvider-CJK-usBy.js";import"./useTheme-Bb3Mws8A.js";const b={title:"Layout/Container",component:r,tags:["autodocs"]},t={argTypes:{maxWidth:{control:"radio",table:{defaultValue:{summary:"lg"}}},fixed:{control:"boolean",table:{defaultValue:{summary:"false"}}}},parameters:{docs:{description:{story:"The Container component centers your content horizontally. It's the most basic layout element and often used for the main layout orientation. It constrains the content width which is adjustable via the 'maxWidth' prop."}}},render:n=>s(r,{...n,style:{backgroundColor:"grey",height:140}})};var e,o,a;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  argTypes: {
+    maxWidth: {
+      control: "radio",
+      table: {
+        defaultValue: {
+          summary: "lg"
+        }
+      }
+    },
+    fixed: {
+      control: "boolean",
+      table: {
+        defaultValue: {
+          summary: "false"
+        }
+      }
+    }
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: \`The Container component centers your content horizontally. It's the most basic layout element and often used for the main layout orientation. It constrains the content width which is adjustable via the 'maxWidth' prop.\`
+      }
+    }
+  },
+  render: args => <Container {...args} style={{
+    backgroundColor: "grey",
+    height: 140
+  }} />
+}`,...(a=(o=t.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};const x=["Basic"];export{t as Basic,x as __namedExportsOrder,b as default};
