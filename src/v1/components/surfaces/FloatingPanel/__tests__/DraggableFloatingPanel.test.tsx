7376ab4c482c00343a8dbc739fb203e5c02f558b
Vanessa Kemp <102584595+VeeKemp@users.noreply.github.com>
2025-10-21 14:19:17 +0100

    fix: update fixed panels and tests (#328)
    * fix: update fix panel to close instead of minimize
    * fix: remove dock and dock item

--- src/v1/components/surfaces/FloatingPanel/__tests__/DraggableFloatingPanel.test.tsx
+++ src/v1/components/surfaces/FloatingPanel/__tests__/DraggableFloatingPanel.test.tsx
@@ -6,49 +6,22 @@ import * as floatingPanelContext from "../useFloatingPanelContext";
 import DraggableFloatingPanel from "../DraggableFloatingPanel";
 
 const renderComponent = (count?: number) => {
-  return renderWithUser(
-    <DraggableFloatingPanel
-      title="Geo locations"
-      targetId="map"
-      count={count}
-    />,
-    {
-      wrapper: (props) => <FloatingPanelProvider {...props} />,
-    }
-  );
+  return renderWithUser(<DraggableFloatingPanel title="Geo locations" targetId="map" count={count} />, {
+    wrapper: (props) => <FloatingPanelProvider {...props} />,
+  });
 };
 
 describe("Draggable floating panel component", () => {
   beforeEach(() => {
     jest.restoreAllMocks();
-  })
-
-  test("The toggleMinimised function is called with the value of the targetId prop", async () => {
-    const toggleMinimisedMock = jest.fn();
-    jest
-      .spyOn(floatingPanelContext, "useFloatingPanelContext")
-      .mockReturnValue({
-        toggleMinimised: toggleMinimisedMock,
-        toggleVisibility: jest.fn(),
-        get: jest.fn(),
-        panels: { map: { visible: true, minimised: false }},
-      });
-
-    const { user } = renderComponent();
-
-    await user.click(screen.getByRole("button", { name: "minimise" }));
-    expect(toggleMinimisedMock).toHaveBeenCalledWith("map");
   });
 
   test("renders total count when number is greater than 0", () => {
-    jest
-      .spyOn(floatingPanelContext, "useFloatingPanelContext")
-      .mockReturnValue({
-        toggleMinimised: jest.fn(),
-        toggleVisibility: jest.fn(),
-        get: jest.fn(),
-        panels: { map: { visible: true, minimised: false }},
-      });
+    jest.spyOn(floatingPanelContext, "useFloatingPanelContext").mockReturnValue({
+      toggleVisibility: jest.fn(),
+      get: jest.fn(),
+      panels: { map: { visible: true, minimised: false } },
+    });
     renderComponent(2);
 
     expect(screen.getByText("Geo locations")).toBeVisible();
