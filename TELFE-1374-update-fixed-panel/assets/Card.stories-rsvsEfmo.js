908d3de3e9049b99f8eab4326dec8423883f3ada
VeeKemp <VeeKemp@users.noreply.github.com>
2025-10-21 12:21:32 +0000

    deploy: 10961085e37617dee6fc3eed5ef4b3edf321d33f

---
+++ TELFE-1374-update-fixed-panel/assets/Card.stories-rsvsEfmo.js
@@ -0,0 +1,17 @@
+import{a as e,j as r}from"./emotion-react-jsx-runtime.browser.esm-BgDa5HT4.js";import{C as i}from"./Card-B74XhqI-.js";import{C as s,a as d}from"./CardActions-BsipVerF.js";import{b as c,T as p}from"./Text-BVIdl1Jw.js";import{B as m}from"./Button-2o7EfnUw.js";import"./jsx-runtime-DMAvRu52.js";import"./index-Dl6G-zuu.js";import"./defaultTheme-DzLjz-dB.js";import"./styled-gOU_XONt.js";import"./generateUtilityClasses-Btm8e7dB.js";import"./DefaultPropsProvider-CJK-usBy.js";import"./Paper-BG42kFkz.js";import"./Typography-Dhe6o_9I.js";import"./ButtonBase-DExEX2yL.js";import"./TransitionGroupContext-BU5kpZ0L.js";import"./useEnhancedEffect-DZnfS5Iy.js";import"./Button-B3igqjqf.js";const k={title:"Surfaces/Card",component:i,tags:["autodocs"]},a={args:{variant:"outlined",children:e(d,{children:[r(c,{children:"Capybara"}),r(p,{paragraph:!0,children:"Capybara are the biggest rodents in the world. Our capy family can be seen strutting their stuff in our South American paddock!"}),r(s,{children:r(m,{color:"secondary",variant:"contained",children:"Share"})})]})}};var t,n,o;a.parameters={...a.parameters,docs:{...(t=a.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    variant: "outlined",
+    children: <CardContent>
+        <H1>Capybara</H1>
+        <Text paragraph>
+          Capybara are the biggest rodents in the world. Our capy family can be seen strutting their stuff in our South
+          American paddock!
+        </Text>
+        <CardActions>
+          <Button color="secondary" variant="contained">
+            Share
+          </Button>
+        </CardActions>
+      </CardContent>
+  }
+}`,...(o=(n=a.parameters)==null?void 0:n.docs)==null?void 0:o.source}}};const w=["OutlinedCard"];export{a as OutlinedCard,w as __namedExportsOrder,k as default};
---
+++ TELFE-1374-update-fixed-panel/assets/LinearProgress.stories-C43lqWNZ.js
@@ -0,0 +1,9 @@
+import{L as t}from"./LinearProgress-BvlhFhje.js";import"./emotion-react-jsx-runtime.browser.esm-BgDa5HT4.js";import"./jsx-runtime-DMAvRu52.js";import"./index-Dl6G-zuu.js";import"./defaultTheme-DzLjz-dB.js";import"./styled-gOU_XONt.js";import"./index-BbVSaS1S.js";import"./generateUtilityClasses-Btm8e7dB.js";import"./DefaultPropsProvider-CJK-usBy.js";const g={title:"Feedback/Linear progress",component:t},r={parameters:{docs:{description:{story:"A line"}}}};var e,o,s;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: \`A line\`
+      }
+    }
+  }
+}`,...(s=(o=r.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};const L=["Basic"];export{r as Basic,L as __namedExportsOrder,g as default};
