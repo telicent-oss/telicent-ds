2007cdf867298bf06392e39c49f2a4f32bc8c9c0
Dave Cockrell <dave@telicent.io>
2025-11-12 11:47:19 +0000

    fix: export AuthContext for use in tests

--- src/candidate-packages/authorize-flow/exports.ts
+++ src/candidate-packages/authorize-flow/exports.ts
@@ -2,5 +2,6 @@ export {};
 export * from "./services/broadcastChannelService";
 export * from "./context/AuthProvider";
 export * from "./context/useAuth";
+export * from "./context/AuthContext";
 export * from "./pages/AuthRedirectUri";
 export * from "./index";
