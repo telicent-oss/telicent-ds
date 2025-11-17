5c3a7bf99c66f8e68112ff78ac3edeec7096bb46
davec504 <davec504@users.noreply.github.com>
2025-11-17 16:03:04 +0000

    deploy: 4f95b3e0b4cd362461ba51ddc95123e06fbe6a9d

---
+++ TELFE-1414/fix-login-flow/assets/Card.stories-DXZ8baxE.js
@@ -0,0 +1,17 @@
+import{a as e,j as r}from"./emotion-react-jsx-runtime.browser.esm-BgDa5HT4.js";import{C as i}from"./Card-BWv60QFk.js";import{C as s,a as d}from"./CardActions-CCW8za6b.js";import{b as c,T as p}from"./Text-DnbGH9ax.js";import{B as m}from"./Button-RcpSRAcx.js";import"./jsx-runtime-DMAvRu52.js";import"./index-Dl6G-zuu.js";import"./identifier-CCo8HfxA.js";import"./styled-DiVYluCu.js";import"./generateUtilityClasses-CRyAR7Z0.js";import"./DefaultPropsProvider-BrPVfFhn.js";import"./Paper-B-rBeRIm.js";import"./Typography-DvrCv4HQ.js";import"./ButtonBase-C0n3d_CB.js";import"./TransitionGroupContext-Cbp9jVrs.js";import"./useEnhancedEffect-DZnfS5Iy.js";import"./Button-DPeHgJ80.js";const k={title:"Surfaces/Card",component:i,tags:["autodocs"]},a={args:{variant:"outlined",children:e(d,{children:[r(c,{children:"Capybara"}),r(p,{paragraph:!0,children:"Capybara are the biggest rodents in the world. Our capy family can be seen strutting their stuff in our South American paddock!"}),r(s,{children:r(m,{color:"secondary",variant:"contained",children:"Share"})})]})}};var t,n,o;a.parameters={...a.parameters,docs:{...(t=a.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    variant: "outlined",
+    children: <CardContent>
+        <H1>Capybara</H1>
+        <Text paragraph>
+          Capybara are the biggest rodents in the world. Our capy family can be seen strutting their stuff in our South
+          American paddock!
+        </Text>
+        <CardActions>
+          <Button color="secondary" variant="contained">
+            Share
+          </Button>
+        </CardActions>
+      </CardContent>
+  }
+}`,...(o=(n=a.parameters)==null?void 0:n.docs)==null?void 0:o.source}}};const w=["OutlinedCard"];export{a as OutlinedCard,w as __namedExportsOrder,k as default};
