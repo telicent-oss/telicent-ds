8b1145ad476bb1c173c05050690b0d8d69981fdd
ACoolmanTelicent <ACoolmanTelicent@users.noreply.github.com>
2025-11-19 10:19:28 +0000

    deploy: 7f6a821ca62a8a26602649fa3b09eea8640015b6

---
+++ TELFE-1401/tweaks/assets/Modal.stories-DW1G0nm7.js
@@ -0,0 +1,110 @@
+import{a,j as e,F as i}from"./emotion-react-jsx-runtime.browser.esm-BgDa5HT4.js";import{r as w}from"./index-Dl6G-zuu.js";import{m as p}from"./AuthModal-CqF2FMxH.js";import"./TelicentHorizontalSVG-DP7xLAp3.js";import"./DataSetIcon-CzT3Gt8m.js";import"./UserIcon-B0AVfc7u.js";import"./UserProfile-CJ8B_wRU.js";import{H as C,e as y,T as g}from"./Text-DnbGH9ax.js";import"./Chip-Jqcc-fVu.js";import{D as v}from"./Divider-HWnn5fVZ.js";import"./TreeView-CbQcwoD4.js";import"./UserProfileContent-BkWk_Qx8.js";import"./FlexBox-owEKTX38.js";import{B as D}from"./Button-DPeHgJ80.js";import{B as O}from"./Box-DR7xjwgg.js";import"./jsx-runtime-DMAvRu52.js";import"./constants-Ca0qL1UB.js";import"./iframe-DeCOIa4a.js";import"../sb-preview/runtime.js";import"./index-B0kOWIl9.js";import"./types-JQgdQoTs.js";import"./useTheme-DC9e96ys.js";import"./identifier-CCo8HfxA.js";import"./useTheme-CEgm5wok.js";import"./GlobalStyles-BK1ltzIn.js";import"./GlobalStyles-DdQ_E3CX.js";import"./ButtonBase-C0n3d_CB.js";import"./styled-DiVYluCu.js";import"./generateUtilityClasses-CRyAR7Z0.js";import"./DefaultPropsProvider-BrPVfFhn.js";import"./TransitionGroupContext-Cbp9jVrs.js";import"./useEnhancedEffect-DZnfS5Iy.js";import"./index-CXtVKvFt.js";import"./Popover-CwNlqRg9.js";import"./utils-DW2zNrrJ.js";import"./Modal-DtyJzhmg.js";import"./Paper-B-rBeRIm.js";import"./Select-DK0erMlh.js";import"./Menu-B2LdKtBH.js";import"./useId-CkA8naEk.js";import"./createSvgIcon-CxivGeKF.js";import"./SvgIcon-DDjczThx.js";import"./Popper-C9fmU3s3.js";import"./AdapterDayjs-CwQoj4VY.js";import"./index-BfyspvgH.js";import"./useThemeProps-BTBzXMHi.js";import"./useThemeProps-BFLDat3t.js";import"./Typography-DvrCv4HQ.js";import"./TextField-o0XFC_j5.js";import"./createStyled-9i3u5Rqa.js";import"./IconButton-CL6GPvoG.js";import"./colorManipulator-DBPky6cX.js";import"./DialogContent-B9KKlp61.js";import"./Chip-B7XAvyWn.js";import"./MenuItem-B5gRYKaL.js";import"./dividerClasses-DixuAXrT.js";import"./UIThemeProvider-DFoAygjS.js";import"./ThemeProvider-BPzF9hu_.js";import"./IconButton-CSCqGkQ5.js";import"./FlexGrid-M25hPc65.js";import"./LinearProgress-CRSreEF6.js";import"./Dialog-BWqAUvqf.js";import"./Button-RcpSRAcx.js";import"./LinkButton-CJcpdsZt.js";import"./Container-CFeVW3qn.js";import"./styled-BEKiHWi7.js";import"./TextField-BD4D0GbT.js";import"./Select-zpLqsQub.js";import"./Autocomplete-CgDVGWac.js";import"./DatePicker-DaiJ72SI.js";import"./DateTimePicker-DiZzYWXt.js";import"./Divider-DOeen-K3.js";import"./Card-BWv60QFk.js";import"./CardActions-CCW8za6b.js";import"./Popover-BljOTz_3.js";import"./Paper-BBwh5-3y.js";import"./ErrorFallback-fPXNZKjO.js";import"./ErrorFallbackText-BLCYy_1i.js";import"./ErrorFallbackWrapper-CSpgK55r.js";import"./MapToggleButtonPresentational-DY66EemL.js";import"./Remove-DLpXZYtt.js";import"./Edit-BwFhg13Q.js";const x=`
+Effective Date: 25/10/20241. 
+
+1. Introduction
+
+This agency is committed to ensuring the security and confidentiality of the personal and sensitive data we handle. This policy outlines the procedures and responsibilities for managing and protecting data to comply with relevant laws and regulations.
+
+2. Purpose
+
+The purpose of this policy is to:Ensure the proper handling, protection, and use of data.Comply with applicable data protection laws and regulations.Protect the privacy rights of individuals whose data we handle.
+
+3. Scope
+
+This policy applies to all employees, contractors, and third-party service providers of who have access to, or handle, data.
+
+4. Data Collection
+
+Lawful and Fair Collection: Data must be collected lawfully and fairly, and only for specified, explicit, and legitimate purposes.Consent: Where applicable, data subjects must provide informed consent for the collection and processing of their data.
+
+5. Data Use
+
+Purpose Limitation: Data must be used only for the purposes for which it was collected and not further processed in a manner incompatible with those purposes.Data Minimization: Only the minimum necessary data should be collected and processed.
+
+6. Data Storage
+
+Secure Storage: Data must be stored securely to prevent unauthorized access, loss, or damage. This includes physical and electronic storage measures.Retention Period: Data must be retained only for as long as necessary to fulfill the purposes for which it was collected, or as required by law.
+
+For questions or concerns about this policy or data protection practices, contact John Smiley at 0394 300498.
+
+Approved by:John Smiley
+Head of Data Capture
+The agency
+
+25/03/2024
+`,oo={title:"Utils/Modal",component:p,tags:["autodocs"],argTypes:{onClose:{description:"Callback fired when the modal is closed.",type:"function"},children:{control:!1,description:"Content inside the component",table:{type:{summary:"ReactNode"}}}}},n={render:s=>{const[l,t]=w.useState(!1);return a(i,{children:[e(D,{variant:"contained",onClick:()=>t(!0),children:"Open Modal"}),e(p,{...s,open:l,onClose:(B,o)=>{if(o==="closeButtonClick"||o==="backdropClick"||o==="escapeKeyDown")t(!1);else throw new Error(`Unknown reason: ${o}`)},children:a(i,{children:[e(C,{id:"modal-title",children:"Data handling policy:"}),e(y,{id:"modal-title",children:"Data set 1:"}),e(v,{}),e(O,{sx:{mt:1,overflow:"auto",flex:"1 1 auto"},children:e(g,{whiteSpace:"preserve",children:x})})]})})]})}},r={render:s=>{const[l,t]=w.useState(!1);return a(i,{children:[e(D,{variant:"contained",onClick:()=>t(!0),children:"Open Modal"}),e(p,{...s,open:l,onClose:(B,o)=>{if(o==="closeButtonClick"||o==="backdropClick"||o==="escapeKeyDown")t(!1);else throw new Error(`Unknown reason: ${o}`)},hideCloseButton:!0,children:a(i,{children:[e(C,{id:"modal-title",children:"Data handling policy:"}),e(y,{id:"modal-title",children:"Data set 1:"}),e(v,{}),e(O,{sx:{mt:1,overflow:"auto",flex:"1 1 auto"},children:e(g,{whiteSpace:"preserve",children:x})})]})})]})}};var c,d,m;n.parameters={...n.parameters,docs:{...(c=n.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: args => {
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = (_event: {}, reason: string) => {
+      if (reason === "closeButtonClick" || reason === "backdropClick" || reason === "escapeKeyDown") {
+        setOpen(false);
+      } else {
+        throw new Error(\`Unknown reason: \${reason}\`);
+      }
+    };
+    return <>
+        <Button variant="contained" onClick={handleOpen}>
+          Open Modal
+        </Button>
+        <Modal {...{
+        ...args,
+        open,
+        onClose: handleClose
+      }}>
+          <>
+            <H4 id="modal-title">Data handling policy:</H4>
+            <H6 id="modal-title">Data set 1:</H6>
+            <Divider />
+
+            <Box sx={{
+            mt: 1,
+            overflow: "auto",
+            // Enables scrolling when content is too long
+            flex: "1 1 auto" // Allows the content to grow and shrink appropriately
+          }}>
+              <Text whiteSpace={"preserve"}>{MOCK_DATA_POLICY}</Text>
+            </Box>
+          </>
+        </Modal>
+      </>;
+  }
+}`,...(m=(d=n.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};var h,u,f;r.parameters={...r.parameters,docs:{...(h=r.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: args => {
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = (_event: {}, reason: string) => {
+      if (reason === "closeButtonClick" || reason === "backdropClick" || reason === "escapeKeyDown") {
+        setOpen(false);
+      } else {
+        throw new Error(\`Unknown reason: \${reason}\`);
+      }
+    };
+    return <>
+        <Button variant="contained" onClick={handleOpen}>
+          Open Modal
+        </Button>
+        <Modal {...{
+        ...args,
+        open,
+        onClose: handleClose,
+        hideCloseButton: true
+      }}>
+          <>
+            <H4 id="modal-title">Data handling policy:</H4>
+            <H6 id="modal-title">Data set 1:</H6>
+            <Divider />
+
+            <Box sx={{
+            mt: 1,
+            overflow: "auto",
+            // Enables scrolling when content is too long
+            flex: "1 1 auto" // Allows the content to grow and shrink appropriately
+          }}>
+              <Text whiteSpace={"preserve"}>{MOCK_DATA_POLICY}</Text>
+            </Box>
+          </>
+        </Modal>
+      </>;
+  }
+}`,...(f=(u=r.parameters)==null?void 0:u.docs)==null?void 0:f.source}}};const to=["Example","WithNoCloseButton"];export{n as Example,r as WithNoCloseButton,to as __namedExportsOrder,oo as default};
