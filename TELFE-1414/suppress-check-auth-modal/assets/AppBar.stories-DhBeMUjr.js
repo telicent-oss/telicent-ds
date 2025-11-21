e43b85a4faed3d962260148f2d57c309eee3ea85
davec504 <davec504@users.noreply.github.com>
2025-11-21 17:51:29 +0000

    deploy: 7ba81093d99c3f1cdfac5588352e2ff7b63004d5

---
+++ TELFE-1414/suppress-check-auth-modal/assets/AppBar.stories-DhBeMUjr.js
@@ -0,0 +1,59 @@
+import{j as r,a as H}from"./emotion-react-jsx-runtime.browser.esm-BgDa5HT4.js";import{w as j,u as L}from"./index-ZIyp8o0e.js";import{A as V}from"./TelicentHorizontalSVG-DP7xLAp3.js";import"./DataSetIcon-CzT3Gt8m.js";import"./UserIcon-B0AVfc7u.js";import{U as E}from"./UserProfile-CJ8B_wRU.js";import{U as O,A as P}from"./AuthModal-fsphptA_.js";import{T as t}from"./Text-DnbGH9ax.js";import"./Chip-Jqcc-fVu.js";import"./Divider-HWnn5fVZ.js";import"./TreeView-CbQcwoD4.js";import"./UserProfileContent-BkWk_Qx8.js";import"./FlexBox-owEKTX38.js";import{B as k}from"./Button-CI14C0BZ.js";import"./IconButton-CSCqGkQ5.js";import"./LinkButton-CJcpdsZt.js";import{appList as R}from"./AppSwitch.stories-DSd8Q2BC.js";import"./jsx-runtime-DMAvRu52.js";import"./index-Dl6G-zuu.js";import"./SvgIcon-DDjczThx.js";import"./identifier-CCo8HfxA.js";import"./styled-DiVYluCu.js";import"./generateUtilityClasses-CRyAR7Z0.js";import"./DefaultPropsProvider-BrPVfFhn.js";import"./FlexGrid-M25hPc65.js";import"./useTheme-DC9e96ys.js";import"./useTheme-CEgm5wok.js";import"./Container-CFeVW3qn.js";import"./styled-BEKiHWi7.js";import"./createStyled-9i3u5Rqa.js";import"./useThemeProps-BFLDat3t.js";import"./Box-DR7xjwgg.js";import"./Typography-DvrCv4HQ.js";import"./Paper-B-rBeRIm.js";import"./index-BfyspvgH.js";import"./createSvgIcon-CxivGeKF.js";import"./Modal-DtyJzhmg.js";import"./utils-DW2zNrrJ.js";import"./TransitionGroupContext-Cbp9jVrs.js";import"./useEnhancedEffect-DZnfS5Iy.js";import"./index-B0kOWIl9.js";import"./IconButton-CL6GPvoG.js";import"./ButtonBase-C0n3d_CB.js";import"./Menu-B2LdKtBH.js";import"./index-CXtVKvFt.js";import"./Popover-CwNlqRg9.js";import"./constants-5W9wEbmd.js";import"./iframe-DJwHegQ4.js";import"../sb-preview/runtime.js";import"./types-JQgdQoTs.js";import"./GlobalStyles-BK1ltzIn.js";import"./GlobalStyles-DdQ_E3CX.js";import"./Select-BWOF8ct1.js";import"./useFormControl-CQLCXd-P.js";import"./useId-CkA8naEk.js";import"./Popper-C9fmU3s3.js";import"./AdapterDayjs-BO6djNq3.js";import"./useThemeProps-BTBzXMHi.js";import"./TextField-BdKvhqgF.js";import"./FormHelperText-D2-CeYt9.js";import"./colorManipulator-DBPky6cX.js";import"./DialogContent-B9KKlp61.js";import"./Button-DPeHgJ80.js";import"./Chip-B7XAvyWn.js";import"./MenuItem-B5gRYKaL.js";import"./dividerClasses-DixuAXrT.js";import"./UIThemeProvider-DFoAygjS.js";import"./ThemeProvider-BPzF9hu_.js";import"./LinearProgress-CRSreEF6.js";import"./Dialog-BWqAUvqf.js";import"./TextField-zYoo9ptH.js";import"./Select-Csl_i8IC.js";import"./Autocomplete-B3gZsuks.js";import"./DatePicker-C1Aua9kQ.js";import"./DateTimePicker-BvPbOfvM.js";import"./Divider-DOeen-K3.js";import"./Card-BWv60QFk.js";import"./CardActions-CCW8za6b.js";import"./Popover-BljOTz_3.js";import"./Paper-BBwh5-3y.js";import"./ErrorFallback-fPXNZKjO.js";import"./ErrorFallbackText-BLCYy_1i.js";import"./ErrorFallbackWrapper-CSpgK55r.js";import"./MapToggleButtonPresentational-DY66EemL.js";import"./Remove-DLpXZYtt.js";import"./Edit-BwFhg13Q.js";const xt={title:"Surfaces/AppBar",component:V,tags:["autodocs"],parameters:{docs:{description:{component:"The AppBar component provides a top navigation bar that supports branding, actions, and more."}},layout:"fullscreen"}},a={},o={args:{appName:"Design System"}},e={args:{...o.args,endChild:r(E,{fullName:"Han Solo",children:H(O,{fullName:"Han Solo",children:[r(t,{variant:"subtitle1",children:"Roles"}),r(t,{children:"Smuggler"}),r(t,{children:"Scoundrel"}),r(t,{children:"Hero"})]})})},play:async({canvasElement:D})=>{const I=j(D);await L.click(I.getByRole("button",{name:"user-profile"}))}},i={args:{appName:"Design System",version:"1.2.0"}},s={args:{...o.args,endChild:r(k,{color:"primary",variant:"contained",startIcon:r("i",{className:"fa-solid fa-arrow-right-from-bracket"}),children:"Sign Out"})}},n={args:{...o.args,startChild:r(P,{apps:R}),endChild:r(k,{color:"primary",variant:"contained",startIcon:r("i",{className:"fa-solid fa-arrow-right-from-bracket"}),children:"Sign Out"})}},p={args:{...o.args,disableBrand:!0,startChild:r(P,{apps:R}),endChild:r(E,{fullName:"Han Solo",children:H(O,{fullName:"Han Solo",children:[r(t,{variant:"subtitle1",children:"Roles"}),r(t,{children:"Smuggler"}),r(t,{children:"Scoundrel"}),r(t,{children:"Hero"})]})})}};var m,c,l;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:"{}",...(l=(c=a.parameters)==null?void 0:c.docs)==null?void 0:l.source}}};var d,u,h;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    appName: "Design System"
+  }
+}`,...(h=(u=o.parameters)==null?void 0:u.docs)==null?void 0:h.source}}};var g,S,f;e.parameters={...e.parameters,docs:{...(g=e.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    ...WithAppName.args,
+    endChild: <UserProfile fullName="Han Solo">
+        <UserStatus fullName="Han Solo">
+          <Text variant="subtitle1">Roles</Text>
+          <Text>Smuggler</Text>
+          <Text>Scoundrel</Text>
+          <Text>Hero</Text>
+        </UserStatus>
+      </UserProfile>
+  },
+  play: async ({
+    canvasElement
+  }) => {
+    const canvas = within(canvasElement);
+    await userEvent.click(canvas.getByRole("button", {
+      name: "user-profile"
+    }));
+  }
+}`,...(f=(S=e.parameters)==null?void 0:S.docs)==null?void 0:f.source}}};var N,x,v;i.parameters={...i.parameters,docs:{...(N=i.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  args: {
+    appName: "Design System",
+    version: "1.2.0"
+  }
+}`,...(v=(x=i.parameters)==null?void 0:x.docs)==null?void 0:v.source}}};var T,B,b;s.parameters={...s.parameters,docs:{...(T=s.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  args: {
+    ...WithAppName.args,
+    endChild: <Button color="primary" variant="contained" startIcon={<i className="fa-solid fa-arrow-right-from-bracket" />}>
+        Sign Out
+      </Button>
+  }
+}`,...(b=(B=s.parameters)==null?void 0:B.docs)==null?void 0:b.source}}};var A,W,y;n.parameters={...n.parameters,docs:{...(A=n.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  args: {
+    ...WithAppName.args,
+    startChild: <AppSwitch apps={appList} />,
+    endChild: <Button color="primary" variant="contained" startIcon={<i className="fa-solid fa-arrow-right-from-bracket" />}>
+        Sign Out
+      </Button>
+  }
+}`,...(y=(W=n.parameters)==null?void 0:W.docs)==null?void 0:y.source}}};var w,U,C;p.parameters={...p.parameters,docs:{...(w=p.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    ...WithAppName.args,
+    disableBrand: true,
+    startChild: <AppSwitch apps={appList} />,
+    endChild: <UserProfile fullName="Han Solo">
+        <UserStatus fullName="Han Solo">
+          <Text variant="subtitle1">Roles</Text>
+          <Text>Smuggler</Text>
+          <Text>Scoundrel</Text>
+          <Text>Hero</Text>
+        </UserStatus>
+      </UserProfile>
+  }
+}`,...(C=(U=p.parameters)==null?void 0:U.docs)==null?void 0:C.source}}};const vt=["Example","WithAppName","WithUserProfile","WithVersionNumber","WithSignOutButton","WithAppSwitch","WithNoBrand"];export{a as Example,o as WithAppName,n as WithAppSwitch,p as WithNoBrand,s as WithSignOutButton,e as WithUserProfile,i as WithVersionNumber,vt as __namedExportsOrder,xt as default};
