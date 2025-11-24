ae56a2b8ea9f41caa9e6764f4d30c97ab632d68c
ACoolmanTelicent <ACoolmanTelicent@users.noreply.github.com>
2025-11-24 09:54:26 +0000

    deploy: d2e2312d1fc303892db48a97e4ef1b164b45347e

---
+++ TELFE-1401/minisearchbox-disabled/assets/Drawer.stories-DxpuCRN7.js
@@ -0,0 +1,71 @@
+import{j as r,a as y,F as C}from"./emotion-react-jsx-runtime.browser.esm-BgDa5HT4.js";import{R as v}from"./index-Dl6G-zuu.js";import{D as b,u as k,P as B,a as R}from"./AuthModal-Chl-cKie.js";import{B as a}from"./Box-DR7xjwgg.js";import"./jsx-runtime-DMAvRu52.js";import"./Text-DnbGH9ax.js";import"./Typography-DvrCv4HQ.js";import"./identifier-CCo8HfxA.js";import"./styled-DiVYluCu.js";import"./generateUtilityClasses-CRyAR7Z0.js";import"./DefaultPropsProvider-BrPVfFhn.js";import"./constants-oPg4YqZw.js";import"./iframe-7es-Tr9n.js";import"../sb-preview/runtime.js";import"./index-B0kOWIl9.js";import"./types-JQgdQoTs.js";import"./useTheme-DC9e96ys.js";import"./useTheme-CEgm5wok.js";import"./GlobalStyles-BK1ltzIn.js";import"./GlobalStyles-DdQ_E3CX.js";import"./UserProfile-CJ8B_wRU.js";import"./UserIcon-B0AVfc7u.js";import"./SvgIcon-DDjczThx.js";import"./index-BfyspvgH.js";import"./FlexBox-owEKTX38.js";import"./styled-BEKiHWi7.js";import"./createStyled-9i3u5Rqa.js";import"./useThemeProps-BFLDat3t.js";import"./createSvgIcon-CxivGeKF.js";import"./Modal-DtyJzhmg.js";import"./utils-DW2zNrrJ.js";import"./TransitionGroupContext-Cbp9jVrs.js";import"./useEnhancedEffect-DZnfS5Iy.js";import"./IconButton-CL6GPvoG.js";import"./ButtonBase-C0n3d_CB.js";import"./Menu-B2LdKtBH.js";import"./index-CXtVKvFt.js";import"./Popover-CwNlqRg9.js";import"./Paper-B-rBeRIm.js";import"./Select-BWOF8ct1.js";import"./useFormControl-CQLCXd-P.js";import"./useId-CkA8naEk.js";import"./Popper-C9fmU3s3.js";import"./TelicentHorizontalSVG-DP7xLAp3.js";import"./FlexGrid-M25hPc65.js";import"./Container-CFeVW3qn.js";import"./DataSetIcon-CzT3Gt8m.js";import"./AdapterDayjs-BO6djNq3.js";import"./useThemeProps-BTBzXMHi.js";import"./TextField-BdKvhqgF.js";import"./FormHelperText-D2-CeYt9.js";import"./colorManipulator-DBPky6cX.js";import"./DialogContent-B9KKlp61.js";import"./Button-DPeHgJ80.js";import"./Chip-B7XAvyWn.js";import"./MenuItem-B5gRYKaL.js";import"./dividerClasses-DixuAXrT.js";import"./UIThemeProvider-DFoAygjS.js";import"./ThemeProvider-BPzF9hu_.js";import"./IconButton-CSCqGkQ5.js";import"./Chip-Jqcc-fVu.js";import"./Divider-HWnn5fVZ.js";import"./Divider-DOeen-K3.js";import"./TreeView-CbQcwoD4.js";import"./UserProfileContent-BkWk_Qx8.js";import"./LinearProgress-CRSreEF6.js";import"./Dialog-BWqAUvqf.js";import"./Button-CI14C0BZ.js";import"./LinkButton-CJcpdsZt.js";import"./TextField-zYoo9ptH.js";import"./Select-Csl_i8IC.js";import"./Autocomplete-B3gZsuks.js";import"./DatePicker-C1Aua9kQ.js";import"./DateTimePicker-BvPbOfvM.js";import"./Card-BWv60QFk.js";import"./CardActions-CCW8za6b.js";import"./Popover-BljOTz_3.js";import"./Paper-BBwh5-3y.js";import"./ErrorFallback-fPXNZKjO.js";import"./ErrorFallbackText-BLCYy_1i.js";import"./ErrorFallbackWrapper-CSpgK55r.js";import"./MapToggleButtonPresentational-DY66EemL.js";import"./Remove-DLpXZYtt.js";import"./Edit-BwFhg13Q.js";const ae={title:"Component Library/Drawer",component:b,tags:["autodocs"],parameters:{docs:{description:{component:"Persistent drawer with toggle control. Communicates open state via callback.\n\nUse `useDrawer` hook and `DrawerPresentational` for imperative control."}}},argTypes:{drawerWidth:{control:{type:"number"},description:"Width in pixels"},onVisibilityChange:{action:"visibilityChanged"}}},e={args:{children:r(a,{p:2,children:"Drawer content"})}},o={args:{drawerWidth:200,children:r(a,{p:2,height:400,children:"Narrow drawer & custom PaperSx"}),PaperSx:{outline:"dashed 3px yellow",bgcolor:"dodgerblue"}}},t={name:"Presentational + useDrawer",parameters:{docs:{description:{story:"Demonstrates imperative API via `useDrawer` hook and `DrawerPresentational`."}}},render:()=>{const i=v.useRef(null),{toggleDrawer:s,drawerProps:p}=k({ref:i,initialOpen:!1});return y(C,{children:[r(B,{onClick:s,children:"Toggle Drawer"}),r(R,{...p,children:r(a,{p:2,children:"Controlled content"})})]})}},n={name:"Presentational + useDrawer: Imperative api only",parameters:{docs:{description:{story:"Demonstrates imperative API via `useDrawer` without a onToggle set (and thus no toggle button)."}}},render:()=>{const i=v.useRef(null),s=k({ref:i,initialOpen:!1}),{onToggle:p,...T}=s.drawerProps;return y(C,{children:[r(B,{onClick:p,children:"Toggle Drawer"}),r(R,{...T,children:r(a,{p:2,children:"Chevron hidden: onClick unset, so clicking does nothing"})})]})}};var m,l,c;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    children: <Box p={2}>Drawer content</Box>
+  }
+}`,...(c=(l=e.parameters)==null?void 0:l.docs)==null?void 0:c.source}}};var d,u,g;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    drawerWidth: 200,
+    children: <Box p={2} height={400}>
+        Narrow drawer & custom PaperSx
+      </Box>,
+    PaperSx: {
+      outline: \`dashed 3px yellow\`,
+      bgcolor: \`dodgerblue\`
+    }
+  }
+}`,...(g=(u=o.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var w,h,D;t.parameters={...t.parameters,docs:{...(w=t.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  name: "Presentational + useDrawer",
+  parameters: {
+    docs: {
+      description: {
+        story: \`Demonstrates imperative API via \\\`useDrawer\\\` hook and \\\`DrawerPresentational\\\`.\`
+      }
+    }
+  },
+  render: () => {
+    // uses hook internally; no ref forwarding for presentational
+    const ctrlRef = React.useRef<DrawerController>(null);
+    const {
+      toggleDrawer,
+      drawerProps
+    } = useDrawer({
+      ref: ctrlRef,
+      initialOpen: false
+    });
+    return <>
+        <PrimaryButton onClick={toggleDrawer}>Toggle Drawer</PrimaryButton>
+
+        <DrawerPresentational {...drawerProps}>
+          <Box p={2}>Controlled content</Box>
+        </DrawerPresentational>
+      </>;
+  }
+}`,...(D=(h=t.parameters)==null?void 0:h.docs)==null?void 0:D.source}}};var P,f,x;n.parameters={...n.parameters,docs:{...(P=n.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  name: "Presentational + useDrawer: Imperative api only",
+  parameters: {
+    docs: {
+      description: {
+        story: \`Demonstrates imperative API via \\\`useDrawer\\\` without a onToggle set (and thus no toggle button).\`
+      }
+    }
+  },
+  render: () => {
+    // uses hook internally; no ref forwarding for presentational
+    const ctrlRef = React.useRef<DrawerController>(null);
+    const drawer = useDrawer({
+      ref: ctrlRef,
+      initialOpen: false
+    });
+    const {
+      onToggle,
+      ...drawerPropsNoToggle
+    } = drawer.drawerProps;
+    return <>
+        <PrimaryButton onClick={onToggle}>Toggle Drawer</PrimaryButton>
+
+        <DrawerPresentational {...drawerPropsNoToggle}>
+          <Box p={2}>Chevron hidden: onClick unset, so clicking does nothing</Box>
+        </DrawerPresentational>
+      </>;
+  }
+}`,...(x=(f=n.parameters)==null?void 0:f.docs)==null?void 0:x.source}}};const ie=["Default","CustomWidth","PresentationalAndHook","ImperativeApi"];export{o as CustomWidth,e as Default,n as ImperativeApi,t as PresentationalAndHook,ie as __namedExportsOrder,ae as default};
