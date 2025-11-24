522972aec23a9ad70c0017b5c85bb6bb154ec4a9
ACoolmanTelicent <ACoolmanTelicent@users.noreply.github.com>
2025-11-24 10:39:05 +0000

    deploy: 86986daccaa1415a30287733bc6a103aafb87d0a

---
+++ prerelease/v1.5.11-TELFE-1401.0/assets/useThemeProps-BTBzXMHi.js
@@ -0,0 +1,1 @@
+import{d as m,T as o}from"./identifier-CCo8HfxA.js";import{u as s}from"./useThemeProps-BFLDat3t.js";function a({props:e,name:r}){return s({props:e,name:r,defaultTheme:m,themeId:o})}export{a as u};
