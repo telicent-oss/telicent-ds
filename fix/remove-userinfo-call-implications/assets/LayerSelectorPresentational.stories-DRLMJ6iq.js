9e678abb894d35b9ad2470ccefc1ccc280e9fc6c
ACoolmanTelicent <ACoolmanTelicent@users.noreply.github.com>
2025-12-05 17:00:35 +0000

    deploy: c96be7879440730182bc374ff24034f92aabb688

---
+++ fix/remove-userinfo-call-implications/assets/LayerSelectorPresentational.stories-DRLMJ6iq.js
@@ -0,0 +1,30 @@
+import{a as p,j as o}from"./emotion-react-jsx-runtime.browser.esm-BgDa5HT4.js";import{b as a,c as n}from"./AuthModal-BKWqlAM_.js";import"./jsx-runtime-DMAvRu52.js";import"./index-Dl6G-zuu.js";import"./Text-DnbGH9ax.js";import"./Typography-DvrCv4HQ.js";import"./identifier-CCo8HfxA.js";import"./styled-DiVYluCu.js";import"./generateUtilityClasses-CRyAR7Z0.js";import"./DefaultPropsProvider-BrPVfFhn.js";import"./constants-wO3OIIKi.js";import"./iframe-Dd2uuJdK.js";import"../sb-preview/runtime.js";import"./index-B0kOWIl9.js";import"./types-JQgdQoTs.js";import"./useTheme-DC9e96ys.js";import"./useTheme-CEgm5wok.js";import"./GlobalStyles-BK1ltzIn.js";import"./GlobalStyles-DdQ_E3CX.js";import"./UserProfile-CJ8B_wRU.js";import"./UserIcon-B0AVfc7u.js";import"./SvgIcon-DDjczThx.js";import"./index-BfyspvgH.js";import"./FlexBox-owEKTX38.js";import"./styled-BEKiHWi7.js";import"./createStyled-9i3u5Rqa.js";import"./useThemeProps-BFLDat3t.js";import"./createSvgIcon-CxivGeKF.js";import"./Modal-DtyJzhmg.js";import"./utils-DW2zNrrJ.js";import"./TransitionGroupContext-Cbp9jVrs.js";import"./useEnhancedEffect-DZnfS5Iy.js";import"./IconButton-CL6GPvoG.js";import"./ButtonBase-C0n3d_CB.js";import"./Menu-B2LdKtBH.js";import"./index-CXtVKvFt.js";import"./Popover-CwNlqRg9.js";import"./Paper-B-rBeRIm.js";import"./Select-BWOF8ct1.js";import"./useFormControl-CQLCXd-P.js";import"./useId-CkA8naEk.js";import"./Popper-C9fmU3s3.js";import"./TelicentHorizontalSVG-DP7xLAp3.js";import"./FlexGrid-M25hPc65.js";import"./Container-CFeVW3qn.js";import"./Box-DR7xjwgg.js";import"./DataSetIcon-CzT3Gt8m.js";import"./AdapterDayjs-BO6djNq3.js";import"./useThemeProps-BTBzXMHi.js";import"./TextField-BdKvhqgF.js";import"./FormHelperText-D2-CeYt9.js";import"./colorManipulator-DBPky6cX.js";import"./DialogContent-B9KKlp61.js";import"./Button-DPeHgJ80.js";import"./Chip-B7XAvyWn.js";import"./MenuItem-B5gRYKaL.js";import"./dividerClasses-DixuAXrT.js";import"./UIThemeProvider-DFoAygjS.js";import"./ThemeProvider-BPzF9hu_.js";import"./IconButton-CSCqGkQ5.js";import"./Chip-Jqcc-fVu.js";import"./Divider-HWnn5fVZ.js";import"./Divider-DOeen-K3.js";import"./TreeView-CbQcwoD4.js";import"./UserProfileContent-BkWk_Qx8.js";import"./LinearProgress-CRSreEF6.js";import"./Dialog-BWqAUvqf.js";import"./Button-CI14C0BZ.js";import"./LinkButton-CJcpdsZt.js";import"./TextField-zYoo9ptH.js";import"./Select-Csl_i8IC.js";import"./Autocomplete-B3gZsuks.js";import"./DatePicker-C1Aua9kQ.js";import"./DateTimePicker-BvPbOfvM.js";import"./Card-BWv60QFk.js";import"./CardActions-CCW8za6b.js";import"./Popover-BljOTz_3.js";import"./Paper-BBwh5-3y.js";import"./ErrorFallback-fPXNZKjO.js";import"./ErrorFallbackText-BLCYy_1i.js";import"./ErrorFallbackWrapper-CSpgK55r.js";import"./MapToggleButtonPresentational-DY66EemL.js";import"./Remove-DLpXZYtt.js";import"./Edit-BwFhg13Q.js";const s=[{uri:"u1",image:"https://dummyimage.com/25x20&text=1",label:"First"},{uri:"u2",image:"https://dummyimage.com/25x20&text=2",label:"Second"},{uri:"u3",image:"https://dummyimage.com/25x20&text=3",label:"Third"}],l=r=>p("div",{children:[o(a,{color:"secondary",variant:"outlined",...r}),o("style",{children:`
+          body {
+            overflow: auto !important;
+          }
+          #layer-selector-popover {
+            pointer-events: none !important;
+          }
+        `}),o(n,{...r})]}),qt={title:"Component Library/Map/primitives/LayerSelectorPresentational",component:l,tags:["autodocs"],parameters:{docs:{description:{component:`
+Renders a fake Presentational LayerSelector to help demonstrate how the default LayerSelector is assembled (used in FeatureMap)
+
+
+<p>
+  👉 Open 
+  <a href="iframe.html?viewMode=docs&id=component-library-map-composites-featuremap--docs" target="_blank">
+    FeatureMap story
+  </a>
+</p>
+
+**NOTE** the PopOver story renders with position absolute, there is some hack styles to unlock the page when popover is visible.
+
+• \`data\`: Array of { uri, image, label }  
+• \`selectedIndex\` & \`anchorEl\`: control popover state  
+• Handlers: \`onClickDropdown\`, \`onCloseDropdown\`, \`onListItemClick\`
+`}}},argTypes:{data:{control:"object"},selectedIndex:{control:{type:"number",min:0,max:2}},anchorEl:{control:!1},onClickDropdown:{action:"dropdownClicked"},onCloseDropdown:{action:"dropdownClosed"},onListItemClick:{action:"itemClicked"}}},t={args:{data:s,selectedIndex:0,anchorEl:document.createElement("button")}};var e,i,m;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    data: sampleData,
+    selectedIndex: 0,
+    anchorEl: document.createElement("button")
+  }
+}`,...(m=(i=t.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};const zt=["Closed"];export{t as Closed,zt as __namedExportsOrder,qt as default};
