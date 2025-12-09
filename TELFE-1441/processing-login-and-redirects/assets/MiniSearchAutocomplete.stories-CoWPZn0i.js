da619a1b2d8240cef4673f36db4708a7d7b7d61b
ACoolmanTelicent <ACoolmanTelicent@users.noreply.github.com>
2025-12-09 22:49:45 +0000

    deploy: 4323e2fb57313e97ee1fecb2ef7d1765afb2b259

---
+++ TELFE-1441/processing-login-and-redirects/assets/MiniSearchAutocomplete.stories-CoWPZn0i.js
@@ -0,0 +1,133 @@
+import{j as e,a as i}from"./emotion-react-jsx-runtime.browser.esm-BgDa5HT4.js";import{r as p}from"./index-Dl6G-zuu.js";import{M as B,f as x,g as O,h as E,i as z,j as L}from"./AuthModal-CUlTxh6F.js";import{P as T}from"./TelicentHorizontalSVG-DP7xLAp3.js";import"./DataSetIcon-CzT3Gt8m.js";import"./UserIcon-B0AVfc7u.js";import{D as d}from"./UserProfile-DRN111Dw.js";import{T as j}from"./Text-DnbGH9ax.js";import"./Chip-Jqcc-fVu.js";import{D as A}from"./Divider-HWnn5fVZ.js";import"./TreeView-B3svt8lB.js";import"./UserProfileContent-BkWk_Qx8.js";import{F as D}from"./FlexBox-owEKTX38.js";import{I as a}from"./IconButton-CSCqGkQ5.js";import"./FlexGrid-M25hPc65.js";import"./Container-CFeVW3qn.js";import{P as J}from"./Popover-CAHG92mn.js";import"./jsx-runtime-DMAvRu52.js";import"./constants-B9G9mcxd.js";import"./iframe-MqSQKm6Q.js";import"../sb-preview/runtime.js";import"./index-Dn0cO_TJ.js";import"./types-JQgdQoTs.js";import"./useTheme-DC9e96ys.js";import"./identifier-CCo8HfxA.js";import"./useTheme-CEgm5wok.js";import"./GlobalStyles-BK1ltzIn.js";import"./GlobalStyles-DdQ_E3CX.js";import"./ButtonBase-C0n3d_CB.js";import"./styled-DiVYluCu.js";import"./generateUtilityClasses-CRyAR7Z0.js";import"./DefaultPropsProvider-BrPVfFhn.js";import"./TransitionGroupContext-Cbp9jVrs.js";import"./useEnhancedEffect-DZnfS5Iy.js";import"./index-CXtVKvFt.js";import"./Popover-DD2HYYg7.js";import"./utils-QfZKikgD.js";import"./Modal-cSJVsbaW.js";import"./Paper-B-rBeRIm.js";import"./Select-Nzfsci8P.js";import"./useFormControl-CQLCXd-P.js";import"./Menu-CYNMhx33.js";import"./useId-CkA8naEk.js";import"./createSvgIcon-CxivGeKF.js";import"./SvgIcon-DDjczThx.js";import"./Popper-QmfZ5sgW.js";import"./AdapterDayjs-Dmcgw6f1.js";import"./index-BfyspvgH.js";import"./useThemeProps-BTBzXMHi.js";import"./useThemeProps-BFLDat3t.js";import"./Typography-DvrCv4HQ.js";import"./TextField-BnU6ehgf.js";import"./FormHelperText-D2-CeYt9.js";import"./createStyled-9i3u5Rqa.js";import"./IconButton-CL6GPvoG.js";import"./colorManipulator-DBPky6cX.js";import"./DialogContent-DyFozIFy.js";import"./Button-DPeHgJ80.js";import"./Chip-B7XAvyWn.js";import"./MenuItem-MWBitPoC.js";import"./dividerClasses-DixuAXrT.js";import"./UIThemeProvider-vkr8-daa.js";import"./ThemeProvider-BPzF9hu_.js";import"./Box-DR7xjwgg.js";import"./LinearProgress-CRSreEF6.js";import"./Dialog-C-jl0jSy.js";import"./Button-CI14C0BZ.js";import"./LinkButton-CJcpdsZt.js";import"./TextField-BXFcgNlD.js";import"./Select-qyD46tLz.js";import"./Autocomplete-2ovWRRWN.js";import"./DatePicker-DSr6TGip.js";import"./DateTimePicker-C633L0Z7.js";import"./Divider-DOeen-K3.js";import"./styled-BEKiHWi7.js";import"./Card-BWv60QFk.js";import"./CardActions-CCW8za6b.js";import"./Paper-BBwh5-3y.js";import"./ErrorFallback-fPXNZKjO.js";import"./ErrorFallbackText-BLCYy_1i.js";import"./ErrorFallbackWrapper-CSpgK55r.js";import"./MapToggleButtonPresentational-DY66EemL.js";import"./Remove-DLpXZYtt.js";import"./Edit-BwFhg13Q.js";const s=n=>e(B,{...n}),uo={title:"Inputs/Search/MiniSearchAutocomplete",component:s,tags:["autodocs"]},t={args:{options:[{id:"telicent-storybook/anakin-solo",label:"Anakin Solo",isRecentSearch:!0},{id:"telicent-storybook/boba-fett",label:"Boba Fett",isRecentSearch:!0},{id:"telicent-storybook/c-3po",label:"C-3PO",isRecentSearch:!0},{id:"telicent-storybook/ben-skywalker",label:"Ben Skywalker",isRecentSearch:!0},{id:"telicent-storybook/durge",label:"Durge",isRecentSearch:!0},{id:"telicent-storybook/cad-bane",label:"Cad Bane",isRecentSearch:!0},{id:"telicent-storybook/darth-vader",label:"Darth Vader",isRecentSearch:!0},{id:"telicent-storybook/chewbacca",label:"Chewbacca",isRecentSearch:!0},{id:"telicent-storybook/han-solo",label:"Han Solo",isRecentSearch:!0},{id:"telicent-storybook/princess-leia",label:"Princess Leia",isRecentSearch:!0},{id:"telicent-storybook/yoda",label:"Yoda",isRecentSearch:!0},{id:"telicent-storybook/quinlan-vos",label:"Quinlan Vos",isRecentSearch:!0}],placeholder:"Search"},render:n=>e(s,{...n})},r={args:{...t.args,options:[...t.args.options,{id:"telicent-storybook/jacen-solo",label:"Jacen Solo",isRecentSearch:!1},{id:"telicent-storybook/jaina-solo",label:"Jaina Solo",isRecentSearch:!1}],endIcon:e(a,{size:"small","aria-label":"toggle pop over",children:e(d,{fontSize:"inherit"})})},render:n=>{const[v,w]=p.useState(null),[l,c]=p.useState(!1),P=[{id:"telicent-storybook/anakin-solo",label:"Anakin Solo",type:"Person",iconClass:"fa-solid fa-person"},{id:"telicent-storybook/han-solo",label:"Han Solo",type:"Person",iconClass:"fa-solid fa-person"},{id:"telicent-storybook/jacen-solo",label:"Jacen Solo",type:"Person",iconClass:"fa-solid fa-person"},{id:"telicent-storybook/jaina-solo",label:"Jaina Solo",type:"Person",iconClass:"fa-solid fa-person"}],I=o=>{w(o.currentTarget),c(!0)},C=()=>{c(o=>!o)};return i("div",{children:[e(s,{...n,endIcon:e(a,{size:"small","aria-label":"toggle pop over",onClick:I,children:e(d,{rotation:l?180:void 0,fontSize:"inherit"})})}),e(J,{id:"search-popover",open:l,anchorEl:v,anchorOrigin:{vertical:"bottom",horizontal:"left"},transformOrigin:{vertical:-10,horizontal:320},width:360,onClose:C,children:i(D,{rowGap:1,children:[e(j,{variant:"subtitle1",children:"Search results"}),e(A,{}),P.map(o=>e(x,{disablePadding:!0,children:i(O,{role:"undefined",disableRipple:!0,children:[e(E,{children:e(z,{backgroundColor:"black",borderColor:"yellow",color:"yellow",iconClass:o.iconClass})}),e(L,{primary:o.label,secondary:o.type}),e(a,{edge:"end","aria-label":"delete",size:"small",children:e(T,{fontSize:"inherit"})})]})},o.id))]})})]})}};var m,h,b,u,S;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    options: [{
+      id: "telicent-storybook/anakin-solo",
+      label: "Anakin Solo",
+      isRecentSearch: true
+    }, {
+      id: "telicent-storybook/boba-fett",
+      label: "Boba Fett",
+      isRecentSearch: true
+    }, {
+      id: "telicent-storybook/c-3po",
+      label: "C-3PO",
+      isRecentSearch: true
+    }, {
+      id: "telicent-storybook/ben-skywalker",
+      label: "Ben Skywalker",
+      isRecentSearch: true
+    }, {
+      id: "telicent-storybook/durge",
+      label: "Durge",
+      isRecentSearch: true
+    }, {
+      id: "telicent-storybook/cad-bane",
+      label: "Cad Bane",
+      isRecentSearch: true
+    }, {
+      id: "telicent-storybook/darth-vader",
+      label: "Darth Vader",
+      isRecentSearch: true
+    }, {
+      id: "telicent-storybook/chewbacca",
+      label: "Chewbacca",
+      isRecentSearch: true
+    }, {
+      id: "telicent-storybook/han-solo",
+      label: "Han Solo",
+      isRecentSearch: true
+    }, {
+      id: "telicent-storybook/princess-leia",
+      label: "Princess Leia",
+      isRecentSearch: true
+    }, {
+      id: "telicent-storybook/yoda",
+      label: "Yoda",
+      isRecentSearch: true
+    }, {
+      id: "telicent-storybook/quinlan-vos",
+      label: "Quinlan Vos",
+      isRecentSearch: true
+    }],
+    placeholder: "Search"
+  },
+  render: (args: Story["args"]) => <StoryExample {...args} />
+}`,...(b=(h=t.parameters)==null?void 0:h.docs)==null?void 0:b.source},description:{story:"Recent searches will be displayed when `isRecentSearch` has been set to true in `options`",...(S=(u=t.parameters)==null?void 0:u.docs)==null?void 0:S.description}}};var y,k,g,f,R;r.parameters={...r.parameters,docs:{...(y=r.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    ...RecentSearches.args,
+    options: [...RecentSearches.args.options, {
+      id: "telicent-storybook/jacen-solo",
+      label: "Jacen Solo",
+      isRecentSearch: false
+    }, {
+      id: "telicent-storybook/jaina-solo",
+      label: "Jaina Solo",
+      isRecentSearch: false
+    }],
+    endIcon: <IconButton size="small" aria-label="toggle pop over">
+        <DownArrowIcon fontSize="inherit" />
+      </IconButton>
+  },
+  render: args => {
+    const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+    const [showPopOver, setShowPopOver] = useState(false);
+    const results = [{
+      id: "telicent-storybook/anakin-solo",
+      label: "Anakin Solo",
+      type: "Person",
+      iconClass: "fa-solid fa-person"
+    }, {
+      id: "telicent-storybook/han-solo",
+      label: "Han Solo",
+      type: "Person",
+      iconClass: "fa-solid fa-person"
+    }, {
+      id: "telicent-storybook/jacen-solo",
+      label: "Jacen Solo",
+      type: "Person",
+      iconClass: "fa-solid fa-person"
+    }, {
+      id: "telicent-storybook/jaina-solo",
+      label: "Jaina Solo",
+      type: "Person",
+      iconClass: "fa-solid fa-person"
+    }];
+    const openPopUp = (event: React.MouseEvent<HTMLButtonElement>) => {
+      setAnchorEl(event.currentTarget);
+      setShowPopOver(true);
+    };
+    const togglePopUp = () => {
+      setShowPopOver(show => !show);
+    };
+    return <div>
+        <StoryExample {...args} endIcon={<IconButton size="small" aria-label="toggle pop over" onClick={openPopUp}>
+              <DownArrowIcon rotation={showPopOver ? 180 : undefined} fontSize="inherit" />
+            </IconButton>} />
+        <PopOver id="search-popover" open={showPopOver} anchorEl={anchorEl} anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "left"
+      }} transformOrigin={{
+        vertical: -10,
+        horizontal: 320
+      }} width={360} onClose={togglePopUp}>
+          <FlexBox rowGap={1}>
+            <Text variant="subtitle1">Search results</Text>
+            <Divider />
+            {results.map(result => <ListItem key={result.id} disablePadding>
+                <ListItemButton role="undefined" disableRipple>
+                  <ListItemIcon>
+                    <IESType backgroundColor="black" borderColor="yellow" color="yellow" iconClass={result.iconClass} />
+                  </ListItemIcon>
+                  <ListItemText primary={result.label} secondary={result.type} />
+                  <IconButton edge="end" aria-label="delete" size="small">
+                    <PlusCircleIcon fontSize="inherit" />
+                  </IconButton>
+                </ListItemButton>
+              </ListItem>)}
+          </FlexBox>
+        </PopOver>
+      </div>;
+  }
+}`,...(g=(k=r.parameters)==null?void 0:k.docs)==null?void 0:g.source},description:{story:`This example shows how suggestions from typeahead and the user's recent
+searches should be displayed. Recent searches should take priority over
+typeahead suggestions. Type solo to the result.`,...(R=(f=r.parameters)==null?void 0:f.docs)==null?void 0:R.description}}};const So=["RecentSearches","SearchResults"];export{t as RecentSearches,r as SearchResults,So as __namedExportsOrder,uo as default};
