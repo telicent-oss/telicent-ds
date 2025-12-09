9a0427f90e018d24aa20ca1f104622388a3f8033
ACoolmanTelicent <ACoolmanTelicent@users.noreply.github.com>
2025-12-09 12:26:44 +0000

    deploy: c4531bb362b349783d178763d02895c36c30872c

---
+++ TELFE-1440/fix-application-type-defaults/assets/index-CXtVKvFt.js
@@ -0,0 +1,1 @@
+import{a as u,_ as n}from"./identifier-CCo8HfxA.js";import{r as o}from"./index-Dl6G-zuu.js";import{j as a}from"./jsx-runtime-DMAvRu52.js";const l=["value"],r=o.createContext();function p(t){let{value:e}=t,s=u(t,l);return a.jsx(r.Provider,n({value:e??!0},s))}const m=()=>{const t=o.useContext(r);return t??!1};export{p as R,m as u};
