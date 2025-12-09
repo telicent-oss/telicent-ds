22ec209fb4f4679b7a0270930c8a9ef259408ca2
davec504 <davec504@users.noreply.github.com>
2025-12-09 10:51:59 +0000

    deploy: 717998efd27a55bb17c896e7438783336c444f65

---
+++ TELFE-1414/polish-callback-page/assets/Checkbox.stories-tzEybUV_.js
@@ -0,0 +1,60 @@
+import{j as m}from"./emotion-react-jsx-runtime.browser.esm-BgDa5HT4.js";import{e as R}from"./AuthModal-C8TLqZkY.js";import{r as j}from"./index-Dl6G-zuu.js";import{B as v}from"./Box-DR7xjwgg.js";import"./jsx-runtime-DMAvRu52.js";import"./Text-DnbGH9ax.js";import"./Typography-DvrCv4HQ.js";import"./identifier-CCo8HfxA.js";import"./styled-DiVYluCu.js";import"./generateUtilityClasses-CRyAR7Z0.js";import"./DefaultPropsProvider-BrPVfFhn.js";import"./constants-xSdysAzn.js";import"./iframe-CSfyQxwS.js";import"../sb-preview/runtime.js";import"./index-Dn0cO_TJ.js";import"./types-JQgdQoTs.js";import"./useTheme-DC9e96ys.js";import"./useTheme-CEgm5wok.js";import"./GlobalStyles-BK1ltzIn.js";import"./GlobalStyles-DdQ_E3CX.js";import"./UserProfile-DRN111Dw.js";import"./UserIcon-B0AVfc7u.js";import"./SvgIcon-DDjczThx.js";import"./index-BfyspvgH.js";import"./FlexBox-owEKTX38.js";import"./styled-BEKiHWi7.js";import"./createStyled-9i3u5Rqa.js";import"./useThemeProps-BFLDat3t.js";import"./createSvgIcon-CxivGeKF.js";import"./Modal-cSJVsbaW.js";import"./utils-QfZKikgD.js";import"./TransitionGroupContext-Cbp9jVrs.js";import"./useEnhancedEffect-DZnfS5Iy.js";import"./IconButton-CL6GPvoG.js";import"./ButtonBase-C0n3d_CB.js";import"./Menu-CYNMhx33.js";import"./index-CXtVKvFt.js";import"./Popover-DD2HYYg7.js";import"./Paper-B-rBeRIm.js";import"./Select-Nzfsci8P.js";import"./useFormControl-CQLCXd-P.js";import"./useId-CkA8naEk.js";import"./Popper-QmfZ5sgW.js";import"./TelicentHorizontalSVG-DP7xLAp3.js";import"./FlexGrid-M25hPc65.js";import"./Container-CFeVW3qn.js";import"./DataSetIcon-CzT3Gt8m.js";import"./AdapterDayjs-Dmcgw6f1.js";import"./useThemeProps-BTBzXMHi.js";import"./TextField-BnU6ehgf.js";import"./FormHelperText-D2-CeYt9.js";import"./colorManipulator-DBPky6cX.js";import"./DialogContent-DyFozIFy.js";import"./Button-DPeHgJ80.js";import"./Chip-B7XAvyWn.js";import"./MenuItem-MWBitPoC.js";import"./dividerClasses-DixuAXrT.js";import"./UIThemeProvider-vkr8-daa.js";import"./ThemeProvider-BPzF9hu_.js";import"./IconButton-CSCqGkQ5.js";import"./Chip-Jqcc-fVu.js";import"./Divider-HWnn5fVZ.js";import"./Divider-DOeen-K3.js";import"./TreeView-B3svt8lB.js";import"./UserProfileContent-BkWk_Qx8.js";import"./LinearProgress-CRSreEF6.js";import"./Dialog-C-jl0jSy.js";import"./Button-CI14C0BZ.js";import"./LinkButton-CJcpdsZt.js";import"./TextField-BXFcgNlD.js";import"./Select-qyD46tLz.js";import"./Autocomplete-2ovWRRWN.js";import"./DatePicker-DSr6TGip.js";import"./DateTimePicker-C633L0Z7.js";import"./Card-BWv60QFk.js";import"./CardActions-CCW8za6b.js";import"./Popover-CAHG92mn.js";import"./Paper-BBwh5-3y.js";import"./ErrorFallback-fPXNZKjO.js";import"./ErrorFallbackText-BLCYy_1i.js";import"./ErrorFallbackWrapper-CSpgK55r.js";import"./MapToggleButtonPresentational-DY66EemL.js";import"./Remove-DLpXZYtt.js";import"./Edit-BwFhg13Q.js";const pr={title:"Inputs/Checkbox",component:R,tags:["autodocs"],parameters:{docs:{description:{component:`
+A simple checkbox component built on Mui's \`<Checkbox>\` with our design-system theming and overrides. It supports the following use cases:
+
+- **Controlled mode:** Pass \`checked\` and \`onChange\` to fully control the checkbox's state.
+- **Uncontrolled mode:** Use the \`defaultChecked\` prop for the initial value and let the checkbox manage its own state.
+
+---
+
+### When & How to use it
+- **Forms:** Use it in any form where you need to allow the user to select multiple or binary options (e.g., terms of service, preferences).
+
+\`\`\`jsx
+<Checkbox 
+  checked={checkedValue} 
+  id="checkbox-demo" 
+  required={true}
+  onChange={handleChange} 
+/>
+\`\`\`
+`}},id:"checkbox-default",ariaLabel:"checkbox"},decorators:e=>m(v,{sx:{margin:"auto"},children:e()})},O=({...e})=>{const[y,D]=j.useState(!1);return m(R,{checked:y,onChange:E=>{D(E.target.checked)},id:"checkbox-demo",...e})},r={args:{checked:!0,id:"checkbox-default",onChange:()=>{}}},o={args:{checked:!1,id:"checkbox-disabled",disabled:!0,onChange:()=>{}}},t={args:{checked:!0,id:"checkbox-custom-label",label:"Custom Label",onChange:()=>{}}},a={args:{checked:!1,id:"checkbox-custom-label",label:"Required",required:!0,onChange:()=>{}}},i={args:{checked:!0,id:"checkbox-custom-label",required:!0,onChange:()=>{}}},s={render:e=>m(O,{...e}),args:{disabled:!1}};var c,n,p;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    checked: true,
+    id: "checkbox-default",
+    onChange: () => {}
+  }
+}`,...(p=(n=r.parameters)==null?void 0:n.docs)==null?void 0:p.source}}};var d,l,h;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    checked: false,
+    id: "checkbox-disabled",
+    disabled: true,
+    onChange: () => {}
+  }
+}`,...(h=(l=o.parameters)==null?void 0:l.docs)==null?void 0:h.source}}};var u,b,g;t.parameters={...t.parameters,docs:{...(u=t.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    checked: true,
+    id: "checkbox-custom-label",
+    label: "Custom Label",
+    onChange: () => {}
+  }
+}`,...(g=(b=t.parameters)==null?void 0:b.docs)==null?void 0:g.source}}};var k,x,C;a.parameters={...a.parameters,docs:{...(k=a.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    checked: false,
+    id: "checkbox-custom-label",
+    label: "Required",
+    required: true,
+    onChange: () => {}
+  }
+}`,...(C=(x=a.parameters)==null?void 0:x.docs)==null?void 0:C.source}}};var f,q,W;i.parameters={...i.parameters,docs:{...(f=i.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    checked: true,
+    id: "checkbox-custom-label",
+    required: true,
+    onChange: () => {}
+  }
+}`,...(W=(q=i.parameters)==null?void 0:q.docs)==null?void 0:W.source}}};var L,S,w;s.parameters={...s.parameters,docs:{...(L=s.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  render: args => <RenderCheckbox {...args} />,
+  args: {
+    disabled: false
+  }
+}`,...(w=(S=s.parameters)==null?void 0:S.docs)==null?void 0:w.source}}};const dr=["Default","Disabled","WithCustomLabel","WithRequired","WithNoLabel","ExampleWithOnChange"];export{r as Default,o as Disabled,s as ExampleWithOnChange,t as WithCustomLabel,i as WithNoLabel,a as WithRequired,dr as __namedExportsOrder,pr as default};
