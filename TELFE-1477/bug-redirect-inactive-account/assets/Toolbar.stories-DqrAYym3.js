115cb2de259b83b032fe5c7373ad9f3fedf0f31b
ACoolmanTelicent <ACoolmanTelicent@users.noreply.github.com>
2026-01-14 15:48:05 +0000

    deploy: 674fb54f0b6c5fa5f3c8badda9333c611b4d6647

---
+++ TELFE-1477/bug-redirect-inactive-account/assets/Toolbar.stories-DqrAYym3.js
@@ -0,0 +1,26 @@
+import{a as r,j as o}from"./emotion-react-jsx-runtime.browser.esm-BgDa5HT4.js";import{f as i}from"./index-edE1QCkR.js";import{T as c,k as x,l as d}from"./AuthModal-DWFDcjBU.js";import"./FlexGrid-M25hPc65.js";import{F as f}from"./TelicentHorizontalSVG-DP7xLAp3.js";import"./Container-CFeVW3qn.js";import{F as e}from"./FlexBox-owEKTX38.js";import{B as m}from"./Button-AYzSydkb.js";import{D as n}from"./UserProfile-DRN111Dw.js";import"./DataSetIcon-CzT3Gt8m.js";import"./UserIcon-B0AVfc7u.js";import"./Text-DnbGH9ax.js";import"./Chip-Jqcc-fVu.js";import{D as p}from"./Divider-HWnn5fVZ.js";import"./TreeView-B3svt8lB.js";import"./UserProfileContent-BkWk_Qx8.js";import"./jsx-runtime-DMAvRu52.js";import"./index-Dl6G-zuu.js";import"./constants-TUGY9cGQ.js";import"./iframe-Bjn8PoZS.js";import"../sb-preview/runtime.js";import"./index-Dn0cO_TJ.js";import"./types-JQgdQoTs.js";import"./useTheme-DC9e96ys.js";import"./identifier-CCo8HfxA.js";import"./useTheme-CEgm5wok.js";import"./GlobalStyles-BK1ltzIn.js";import"./GlobalStyles-DdQ_E3CX.js";import"./ButtonBase-C0n3d_CB.js";import"./styled-DiVYluCu.js";import"./generateUtilityClasses-CRyAR7Z0.js";import"./DefaultPropsProvider-BrPVfFhn.js";import"./TransitionGroupContext-Cbp9jVrs.js";import"./useEnhancedEffect-DZnfS5Iy.js";import"./index-CXtVKvFt.js";import"./Popover-DD2HYYg7.js";import"./utils-QfZKikgD.js";import"./Modal-cSJVsbaW.js";import"./Paper-B-rBeRIm.js";import"./Select-BuWwsDr7.js";import"./FormHelperText-jGmUqac9.js";import"./Menu-CYNMhx33.js";import"./useId-CkA8naEk.js";import"./createSvgIcon-CxivGeKF.js";import"./SvgIcon-DDjczThx.js";import"./Popper-QmfZ5sgW.js";import"./AdapterDayjs-CIaKgf6d.js";import"./index-BfyspvgH.js";import"./useThemeProps-BTBzXMHi.js";import"./useThemeProps-BFLDat3t.js";import"./Typography-DvrCv4HQ.js";import"./TextField-C7Fww-WA.js";import"./createStyled-9i3u5Rqa.js";import"./IconButton-CL6GPvoG.js";import"./colorManipulator-DBPky6cX.js";import"./DialogContent-DyFozIFy.js";import"./Button-DPeHgJ80.js";import"./Chip-B7XAvyWn.js";import"./MenuItem-MWBitPoC.js";import"./dividerClasses-DixuAXrT.js";import"./UIThemeProvider-vkr8-daa.js";import"./ThemeProvider-BPzF9hu_.js";import"./IconButton-CSCqGkQ5.js";import"./Box-DR7xjwgg.js";import"./LinearProgress-CRSreEF6.js";import"./Dialog-C-jl0jSy.js";import"./LinkButton-CJcpdsZt.js";import"./TextField-Bt6j97Dp.js";import"./Select-CXi4ss_i.js";import"./Autocomplete-_HJ_smfh.js";import"./DatePicker-czR37xA_.js";import"./DateTimePicker-BBih8JvK.js";import"./Divider-DOeen-K3.js";import"./styled-BEKiHWi7.js";import"./Card-BWv60QFk.js";import"./CardActions-CCW8za6b.js";import"./Popover-CAHG92mn.js";import"./Paper-BBwh5-3y.js";import"./ErrorFallback-fPXNZKjO.js";import"./ErrorFallbackText-BLCYy_1i.js";import"./ErrorFallbackWrapper-CSpgK55r.js";import"./MapToggleButtonPresentational-DY66EemL.js";import"./Remove-DLpXZYtt.js";import"./Edit-BwFhg13Q.js";const Ro={title:"Surfaces/Toolbar",component:c,tags:["autodocs"]},t={args:{children:r(e,{direction:"row",justifyContent:"space-between",alignItems:"center",sx:{flexGrow:1},children:[o(x,{}),r(e,{direction:"row",alignItems:"center",gap:1,children:[r(d,{size:"small",value:"more-actions",tooltip:"More actions",selected:!1,onClick:()=>i(),children:["More Actions ",o(n,{})]}),o(p,{orientation:"vertical",flexItem:!0}),o(m,{variant:"text",endIcon:o(n,{}),onClick:()=>i(),children:"Saved (1)"}),o(p,{orientation:"vertical",flexItem:!0}),o(m,{color:"inherit",variant:"text",endIcon:o(f,{}),onClick:()=>i(),sx:{ml:1},children:"Save"})]})]})}};var a,l,s;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    children: <FlexBox direction="row" justifyContent="space-between" alignItems="center" sx={{
+      flexGrow: 1
+    }}>
+        <MiniSearchBox />
+
+        <FlexBox direction="row" alignItems="center" gap={1}>
+          <TooltipToggleButton size="small" value="more-actions" tooltip="More actions" selected={false} onClick={() => fn()}>
+            More Actions <DownArrowIcon />
+          </TooltipToggleButton>
+          <Divider orientation="vertical" flexItem />
+          <Button variant="text" endIcon={<DownArrowIcon />} onClick={() => fn()}>
+            Saved (1)
+          </Button>
+          <Divider orientation="vertical" flexItem />
+
+          <Button color="inherit" variant="text" endIcon={<FloppyDiskIcon />} onClick={() => fn()} sx={{
+          ml: 1
+        }}>
+            Save
+          </Button>
+        </FlexBox>
+      </FlexBox>
+  }
+}`,...(s=(l=t.parameters)==null?void 0:l.docs)==null?void 0:s.source}}};const Uo=["Example"];export{t as Example,Uo as __namedExportsOrder,Ro as default};
