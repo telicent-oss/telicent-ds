3b6e65a45d21298b35eb00696448991883666c74
A Coolman <153610820+ACoolmanTelicent@users.noreply.github.com>
2026-01-22 14:51:20 +0000

    feat(Dialog): [TELFE-883] export children (#432)

--- src/v1/components/feedback/index.ts
+++ src/v1/components/feedback/index.ts
@@ -1,4 +1,4 @@
 export { default as LinearProgress } from "./LinearProgress/LinearProgress";
 export { default as Dialog } from "./Dialog/Dialog";
-
+export * from "./Dialog/Dialog";
 export { Skeleton } from "./Skeleton/Skeleton";
