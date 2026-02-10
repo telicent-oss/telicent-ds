e31decd2bd64a6ce947ec8646f9cafbf4ea1a810
Dave Cockrell <dave@telicent.io>
2026-02-10 13:13:28 +0000

    chore: remove console log

--- src/component-library/Map/primitives/ResultsMarkers/ResultsMarkers.tsx
+++ src/component-library/Map/primitives/ResultsMarkers/ResultsMarkers.tsx
@@ -46,7 +46,6 @@ export const ResultsMarkers: React.FC<ResultsMarkersProps> = ({
   findByClassUri,
   onClick
 }) => {
-  console.log({ markers });
   return (
     <Source
       id="document-locations"
