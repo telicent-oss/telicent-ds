07fed33ccd4feb1291ca774e4f38b969b0ac8c37
VeeKemp <VeeKemp@users.noreply.github.com>
2026-02-18 14:57:08 +0000

    deploy: 288626e27e6a561e26393a55f475f666a9d82deb

---
+++ TELFE-914-appbar-ui-changes/assets/Divider.stories-DwVkG2hf.js
@@ -0,0 +1,33 @@
+import{a as o,j as r}from"./emotion-react-jsx-runtime.browser.esm-BgDa5HT4.js";import{D as s}from"./Divider-HWnn5fVZ.js";import{C as D}from"./Card-BWv60QFk.js";import{H as I,a as y,T as p}from"./Text-DnbGH9ax.js";import{F as a}from"./FlexBox-owEKTX38.js";import{C as n}from"./Chip-Jqcc-fVu.js";import{B as z,F as T}from"./TelicentHorizontalSVG-BM77MPHw.js";import"./DataSetIcon-CzT3Gt8m.js";import"./UserIcon-B0AVfc7u.js";import{B as w}from"./Box-DR7xjwgg.js";import"./jsx-runtime-DMAvRu52.js";import"./index-Dl6G-zuu.js";import"./Divider-DOeen-K3.js";import"./identifier-CCo8HfxA.js";import"./styled-DiVYluCu.js";import"./dividerClasses-DixuAXrT.js";import"./generateUtilityClasses-CRyAR7Z0.js";import"./DefaultPropsProvider-BrPVfFhn.js";import"./Paper-B-rBeRIm.js";import"./Typography-DvrCv4HQ.js";import"./styled-BEKiHWi7.js";import"./createStyled-9i3u5Rqa.js";import"./useThemeProps-BFLDat3t.js";import"./useTheme-CEgm5wok.js";import"./Chip-B7XAvyWn.js";import"./createSvgIcon-CxivGeKF.js";import"./SvgIcon-DDjczThx.js";import"./TransitionGroupContext-Cbp9jVrs.js";import"./useEnhancedEffect-DZnfS5Iy.js";import"./ButtonBase-C0n3d_CB.js";import"./FlexGrid-M25hPc65.js";import"./useTheme-DC9e96ys.js";import"./Container-CFeVW3qn.js";const pr={title:"Data display/Divider",component:s,tags:["autodocs"],parameters:{docs:{description:{component:"The Divider component provides a thin, unobtrusive line for grouping elements to reinforce visual hierarchy."}}}},t={render:()=>o(D,{maxWidth:360,children:[o(a,{padding:2,children:[r(I,{children:"Rivers in Africa"}),r(y,{children:"Nile"}),r(p,{children:"The Nile is the longest river in Africa. Its length is around 6,650 kilometers, and its drainage basin covers 11 African countries."})]}),r(s,{}),o(w,{padding:2,children:[r(p,{children:"Other rivers"}),o(a,{direction:"row",spacing:1,children:[r(n,{label:"Zambezi",size:"small"}),r(n,{label:"Niger",size:"small"}),r(n,{label:"Mara",size:"small"})]})]})]})},e={args:{orientation:"vertical",flexItem:!0},render:C=>o(a,{direction:"row",spacing:1,border:1,width:"fit-content",borderColor:"grey",borderRadius:1,padding:1,children:[r(z,{}),r(s,{...C}),r(T,{})]})},i={...e};var d,m,l;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: () => <Card maxWidth={360}>
+      <FlexBox padding={2}>
+        <H4>Rivers in Africa</H4>
+        <H5>Nile</H5>
+        <Text>
+          The Nile is the longest river in Africa. Its length is around 6,650
+          kilometers, and its drainage basin covers 11 African countries.
+        </Text>
+      </FlexBox>
+      <Divider />
+      <Box padding={2}>
+        <Text>Other rivers</Text>
+        <FlexBox direction="row" spacing={1}>
+          <Chip label="Zambezi" size="small" />
+          <Chip label="Niger" size="small" />
+          <Chip label="Mara" size="small" />
+        </FlexBox>
+      </Box>
+    </Card>
+}`,...(l=(m=t.parameters)==null?void 0:m.docs)==null?void 0:l.source}}};var c,h,x,g,f;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    orientation: "vertical",
+    flexItem: true
+  },
+  render: args => <FlexBox direction="row" spacing={1} border={1} width="fit-content" borderColor="grey" borderRadius={1} padding={1}>
+      <BinIcon />
+      <Divider {...args} />
+      <FloppyDiskIcon />
+    </FlexBox>
+}`,...(x=(h=e.parameters)==null?void 0:h.docs)==null?void 0:x.source},description:{story:"Use the orientation prop to change the Divider from `horizontal` to `vertical`.",...(f=(g=e.parameters)==null?void 0:g.docs)==null?void 0:f.description}}};var u,v,b,B,F;i.parameters={...i.parameters,docs:{...(u=i.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  ...Orientation
+}`,...(b=(v=i.parameters)==null?void 0:v.docs)==null?void 0:b.source},description:{story:"Use the `flexItem` prop to display the Divider when it's being used in a flex container.",...(F=(B=i.parameters)==null?void 0:B.docs)==null?void 0:F.description}}};const dr=["Example","Orientation","FlexItem"];export{t as Example,i as FlexItem,e as Orientation,dr as __namedExportsOrder,pr as default};
