d75c658dd63d32f5020c81ed5b24fa17ac3b330b
VeeKemp <VeeKemp@users.noreply.github.com>
2026-02-19 10:39:46 +0000

    deploy: af8b96d693e140c5959b262d9aa0dc361f76e1c9

---
+++ TELFE-914-appbar-ui-changes/assets/AppSwitch.stories-CHkuK4F0.js
@@ -0,0 +1,30 @@
+import{A as e}from"./AuthModal-D5jWDCcC.js";import"./emotion-react-jsx-runtime.browser.esm-BgDa5HT4.js";import"./jsx-runtime-DMAvRu52.js";import"./index-Dl6G-zuu.js";import"./Text-BdkxszJB.js";import"./Typography-DvrCv4HQ.js";import"./identifier-CCo8HfxA.js";import"./styled-DiVYluCu.js";import"./generateUtilityClasses-CRyAR7Z0.js";import"./DefaultPropsProvider-BrPVfFhn.js";import"./constants-CKOD6mIC.js";import"./iframe-DI3_kVhW.js";import"../sb-preview/runtime.js";import"./index-Dn0cO_TJ.js";import"./types-JQgdQoTs.js";import"./useTheme-DC9e96ys.js";import"./useTheme-CEgm5wok.js";import"./GlobalStyles-BK1ltzIn.js";import"./GlobalStyles-DdQ_E3CX.js";import"./UserProfile-BtiM6bQr.js";import"./UserIcon-B0AVfc7u.js";import"./SvgIcon-DDjczThx.js";import"./index-BfyspvgH.js";import"./FlexBox-owEKTX38.js";import"./styled-BEKiHWi7.js";import"./createStyled-9i3u5Rqa.js";import"./useThemeProps-BFLDat3t.js";import"./createSvgIcon-CxivGeKF.js";import"./Modal-cSJVsbaW.js";import"./utils-QfZKikgD.js";import"./TransitionGroupContext-Cbp9jVrs.js";import"./useEnhancedEffect-DZnfS5Iy.js";import"./IconButton-CL6GPvoG.js";import"./ButtonBase-C0n3d_CB.js";import"./Menu-CYNMhx33.js";import"./index-CXtVKvFt.js";import"./Popover-DD2HYYg7.js";import"./Paper-B-rBeRIm.js";import"./Select-8VTLrbff.js";import"./useId-CkA8naEk.js";import"./Popper-QmfZ5sgW.js";import"./TelicentHorizontalSVG-CLA3i2cx.js";import"./FlexGrid-cELkpYdP.js";import"./Container-CFeVW3qn.js";import"./Box-DR7xjwgg.js";import"./DataSetIcon-CzT3Gt8m.js";import"./AdapterDayjs-D7vkZCXk.js";import"./useThemeProps-BTBzXMHi.js";import"./TextField-BU2nRZ6A.js";import"./colorManipulator-DBPky6cX.js";import"./DialogContent-DyFozIFy.js";import"./Button-DPeHgJ80.js";import"./Chip-B7XAvyWn.js";import"./MenuItem-MWBitPoC.js";import"./dividerClasses-DixuAXrT.js";import"./UIThemeProvider-BLvKa1LM.js";import"./ThemeProvider-BPzF9hu_.js";import"./IconButton-CSCqGkQ5.js";import"./Chip-Jqcc-fVu.js";import"./Divider-HWnn5fVZ.js";import"./Divider-DOeen-K3.js";import"./TreeView-B3svt8lB.js";import"./UserProfileContent-BkWk_Qx8.js";import"./LinearProgress-CRSreEF6.js";import"./Dialog-C-jl0jSy.js";import"./Button-RcpSRAcx.js";import"./LinkButton-CJcpdsZt.js";import"./TextField-DztZaD0H.js";import"./Select-D4OJP2ga.js";import"./Autocomplete-DQ7I-sdn.js";import"./DatePicker-DvTHUv-u.js";import"./DateTimePicker-dN1NYyu9.js";import"./Card-BWv60QFk.js";import"./CardActions-pj93ZpKu.js";import"./Popover-CAHG92mn.js";import"./Paper-BBwh5-3y.js";import"./ErrorFallback-fPXNZKjO.js";import"./ErrorFallbackText-BLCYy_1i.js";import"./ErrorFallbackWrapper-CSpgK55r.js";import"./MapToggleButtonPresentational-DY66EemL.js";import"./Remove-DLpXZYtt.js";import"./Edit-BwFhg13Q.js";const t=[{id:"telicent-graph",name:"graph",url:"https://graph.io",icon:"./src/assets/icons/graph.svg"},{id:"telicent-search",name:"search",url:"https://search.io",icon:"./src/assets/icons/search.svg"},{id:"telicent-catalog",name:"catalog",url:"https://data-catalog.io",icon:"./src/assets/icons/catalog.svg"},{id:"telicent-user-portal",name:"portal",url:"https://data-user-portal.io",icon:"./src/assets/icons/user-portal-light.svg"},{id:"telicent-user-portal",name:"portal",url:"https://data-user-portal.io",icon:"./src/assets/icons/user-portal-dark.svg"}],Ht={title:"Data display/App Switch",component:e,tags:["autodocs"],args:{apps:t},parameters:{docs:{description:{component:"ApppSwitch component accepts apps, an array of objects, with id, name, url and icon."}},layout:"fullscreen"}},r={args:{apps:t}};var o,p,i;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`[{
+  id: "telicent-graph",
+  name: "graph",
+  url: "https://graph.io",
+  icon: "./src/assets/icons/graph.svg"
+}, {
+  id: "telicent-search",
+  name: "search",
+  url: "https://search.io",
+  icon: "./src/assets/icons/search.svg"
+}, {
+  id: "telicent-catalog",
+  name: "catalog",
+  url: "https://data-catalog.io",
+  icon: "./src/assets/icons/catalog.svg"
+}, {
+  id: "telicent-user-portal",
+  name: "portal",
+  url: "https://data-user-portal.io",
+  icon: "./src/assets/icons/user-portal-light.svg"
+}, {
+  id: "telicent-user-portal",
+  name: "portal",
+  url: "https://data-user-portal.io",
+  icon: "./src/assets/icons/user-portal-dark.svg"
+}]`,...(i=(p=t.parameters)==null?void 0:p.docs)==null?void 0:i.source}}};var s,a,m;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    apps: appList
+  }
+}`,...(m=(a=r.parameters)==null?void 0:a.docs)==null?void 0:m.source}}};const It=["appList","AppSwitchExample"];export{r as AppSwitchExample,It as __namedExportsOrder,t as appList,Ht as default};
