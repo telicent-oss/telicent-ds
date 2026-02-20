e1bda55bffc3e5e521674f55bd580c9fe8b03db0
Vanessa Kemp <vanessa.kemp@telicent.io>
2026-02-20 13:45:18 +0000

    fix: update chip props

--- src/v1/components/data-display/Chip/Chip.tsx
+++ src/v1/components/data-display/Chip/Chip.tsx
@@ -3,20 +3,11 @@ import MUIChip, { ChipProps as MUIChipProps } from "@mui/material/Chip";
 
 export type ChipProps = Omit<
   MUIChipProps,
-  | "avatar"
-  | "children"
-  | "classes"
-  | "clickable"
-  | "color"
-  | "component"
-  | "deleteIcon"
-  | "icon"
-  | "skipFocusWhenDisabled"
-  | "sx"
+  "avatar" | "children" | "classes" | "clickable" | "component" | "deleteIcon" | "icon" | "skipFocusWhenDisabled"
 >;
 
-const Chip: React.FC<ChipProps> = ({ variant = "filled", ...chipProps }) => (
-  <MUIChip variant={variant} color="primary" {...chipProps} />
+const Chip: React.FC<ChipProps> = ({ variant = "filled", color = "primary", ...chipProps }) => (
+  <MUIChip variant={variant} color={color} {...chipProps} />
 );
 
 export default Chip;
