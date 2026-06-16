e69d36d1550bfbb5c32207d9c306a5fabaf70189
davec504 <davec504@users.noreply.github.com>
2026-06-16 12:23:58 +0000

    deploy: cd3e30f198d817c799d65ed614009b5dfa3fba16

---
+++ chore/remove-acled-refs/assets/AppSwitch.stories-BKtxDaDX.js
@@ -0,0 +1,30 @@
+import{A as m}from"./DropdownButton-CSxA164s.js";import"./emotion-react-jsx-runtime.browser.esm-OTCsP_zj.js";import"./jsx-runtime-BjG_zV1W.js";import"./index-7LYIoTbn.js";import"./UserIcon-Cn55tEQ2.js";import"./createSvgIcon-DU2rVRio.js";import"./defaultTheme-ACnAOKMv.js";import"./styled-Dr6LHSty.js";import"./generateUtilityClasses-CBlNI-gU.js";import"./DefaultPropsProvider-BINl2W1D.js";import"./useTheme-BCfSMqPY.js";import"./useTheme-DmgbyzQG.js";import"./Container-Cpr-ywPw.js";import"./styled-xzbhM0n_.js";import"./createStyled-CsLhtPiI.js";import"./useThemeProps-DhNaUrQ2.js";import"./Stack-_9FQNGpJ.js";import"./Box-BVltnmec.js";import"./Typography-CSCiAqzj.js";import"./Paper-3rHlC0dZ.js";import"./useThemeProps-k0ivW4aO.js";import"./Text-D0lNsqSI.js";import"./AdapterDayjs-Cl81F8SA.js";import"./index-B4OJPA8G.js";import"./Modal-JNcevMIf.js";import"./utils-CXYPeQrV.js";import"./TransitionGroupContext-itkx9jux.js";import"./useEnhancedEffect-oYjd7u20.js";import"./index-DONRZHi-.js";import"./Select-BfRenVFP.js";import"./useFormControl-lkbi0or6.js";import"./useId-Cf3J66lm.js";import"./GlobalStyles-B130YdEm.js";import"./GlobalStyles-Canp8JzW.js";import"./TextField-BklvXUyq.js";import"./IconButton-K5H0oyhF.js";import"./ButtonBase-D1U61Dnt.js";import"./colorManipulator-4CHxWgpo.js";import"./DialogContent-CX0jwU1P.js";import"./Button-6gIpOcku.js";import"./Chip-D4rXNsqT.js";import"./MenuItem-bua5XKnv.js";import"./dividerClasses-Cw9q1-aC.js";import"./UIThemeProvider-4LUh-fR0.js";import"./types-CMP91hxZ.js";import"./ThemeProvider-BUQO26gH.js";import"./Chip-BwAmCpCJ.js";import"./Divider-wVQl8Tit.js";import"./Divider-A3I7BnBV.js";import"./TreeView-CTXybiJL.js";import"./LinearProgress-Bb8hrd7I.js";import"./Spinner-BOxs5Djo.js";import"./Dialog-vILqERHu.js";import"./MapToggleButtonPresentational-86Mslv2I.js";import"./Remove-B14EzRR3.js";import"./TextField-jr6Bp6UA.js";import"./Select-CyedWs7E.js";import"./Switch-CckCM0FO.js";import"./DatePicker-DcCgiJ2j.js";import"./DateTimePicker-Dp4MmL11.js";import"./LinkButton-D9Uz2SFG.js";import"./Paper-CmfYDRfa.js";import"./ErrorFallback-Box0VFpD.js";import"./ErrorFallbackText-BxC3bExW.js";import"./ErrorFallbackWrapper-Dv-Jy562.js";import"./Brand-DN1iXAq-.js";import"./constants-CvdKz64T.js";import"./iframe-CgvgFwUT.js";import"../sb-preview/runtime.js";import"./Edit-DKn8TmSo.js";const t=[{id:"telicent-graph",name:"graph",url:"https://graph.io",icon:"./src/assets/icons/graph.svg"},{id:"telicent-search",name:"search",url:"https://search.io",icon:"./src/assets/icons/search.svg"},{id:"telicent-catalog",name:"catalog",url:"https://data-catalog.io",icon:"./src/assets/icons/catalog.svg"},{id:"telicent-user-portal",name:"portal",url:"https://data-user-portal.io",icon:"./src/assets/icons/user-portal-light.svg"},{id:"telicent-user-portal",name:"portal",url:"https://data-user-portal.io",icon:"./src/assets/icons/user-portal-dark.svg"}],kt={title:"Data display/App Switch",component:m,tags:["autodocs"],args:{apps:t},parameters:{docs:{description:{component:"ApppSwitch component accepts apps, an array of objects, with id, name, url and icon."}},layout:"fullscreen"}},r={args:{apps:t}};var o,p,i;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`[{
+  id: "telicent-graph",
+  name: "graph",
+  url: "https://graph.io",
+  icon: "./src/assets/icons/graph.svg"
+}, {
+  id: "telicent-search",
+  name: "search",
+  url: "https://search.io",
+  icon: "./src/assets/icons/search.svg"
+}, {
+  id: "telicent-catalog",
+  name: "catalog",
+  url: "https://data-catalog.io",
+  icon: "./src/assets/icons/catalog.svg"
+}, {
+  id: "telicent-user-portal",
+  name: "portal",
+  url: "https://data-user-portal.io",
+  icon: "./src/assets/icons/user-portal-light.svg"
+}, {
+  id: "telicent-user-portal",
+  name: "portal",
+  url: "https://data-user-portal.io",
+  icon: "./src/assets/icons/user-portal-dark.svg"
+}]`,...(i=(p=t.parameters)==null?void 0:p.docs)==null?void 0:i.source}}};var s,a,e;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    apps: appList
+  }
+}`,...(e=(a=r.parameters)==null?void 0:a.docs)==null?void 0:e.source}}};const _t=["appList","AppSwitchExample"];export{r as AppSwitchExample,_t as __namedExportsOrder,t as appList,kt as default};
---
+++ chore/remove-acled-refs/assets/InsetInMap.stories-CKg-L9GA.js
@@ -0,0 +1,42 @@
+import{j as t,a}from"./emotion-react-jsx-runtime.browser.esm-OTCsP_zj.js";import{R as h}from"./index-7LYIoTbn.js";import{r as p,s as x,o as u,t as I}from"./DropdownButton-CSxA164s.js";import"./UserIcon-Cn55tEQ2.js";import"./Text-D0lNsqSI.js";import"./Chip-BwAmCpCJ.js";import"./Divider-wVQl8Tit.js";import"./TreeView-CTXybiJL.js";import"./LinearProgress-Bb8hrd7I.js";import"./Spinner-BOxs5Djo.js";import"./Dialog-vILqERHu.js";import"./TextField-jr6Bp6UA.js";import"./Select-CyedWs7E.js";import"./Switch-CckCM0FO.js";import"./DatePicker-DcCgiJ2j.js";import"./DateTimePicker-Dp4MmL11.js";import"./LinkButton-D9Uz2SFG.js";import{a as y}from"./Container-Cpr-ywPw.js";import"./Paper-CmfYDRfa.js";import"./UIThemeProvider-4LUh-fR0.js";import"./ErrorFallback-Box0VFpD.js";import"./ErrorFallbackText-BxC3bExW.js";import"./ErrorFallbackWrapper-Dv-Jy562.js";import"./Brand-DN1iXAq-.js";import{M as B}from"./constants-CvdKz64T.js";import{B as R,a as C,M}from"./MapToggleButtonPresentational-86Mslv2I.js";import{allArgs as b}from"./BasicMap.stories-CTPJzYrE.js";import{i as A}from"./storyconfig-DEIJDHKx.js";import{B as e}from"./Box-BVltnmec.js";import{P as S}from"./Paper-3rHlC0dZ.js";import{T}from"./Typography-CSCiAqzj.js";import{D as v}from"./Divider-A3I7BnBV.js";import{d as w}from"./dividerClasses-Cw9q1-aC.js";import{S as L}from"./Stack-_9FQNGpJ.js";import"./jsx-runtime-BjG_zV1W.js";import"./useThemeProps-k0ivW4aO.js";import"./defaultTheme-ACnAOKMv.js";import"./useThemeProps-DhNaUrQ2.js";import"./DefaultPropsProvider-BINl2W1D.js";import"./useTheme-DmgbyzQG.js";import"./useTheme-BCfSMqPY.js";import"./AdapterDayjs-Cl81F8SA.js";import"./styled-Dr6LHSty.js";import"./index-B4OJPA8G.js";import"./Modal-JNcevMIf.js";import"./utils-CXYPeQrV.js";import"./TransitionGroupContext-itkx9jux.js";import"./useEnhancedEffect-oYjd7u20.js";import"./index-DONRZHi-.js";import"./generateUtilityClasses-CBlNI-gU.js";import"./Select-BfRenVFP.js";import"./useFormControl-lkbi0or6.js";import"./useId-Cf3J66lm.js";import"./createSvgIcon-DU2rVRio.js";import"./GlobalStyles-B130YdEm.js";import"./GlobalStyles-Canp8JzW.js";import"./TextField-BklvXUyq.js";import"./createStyled-CsLhtPiI.js";import"./IconButton-K5H0oyhF.js";import"./ButtonBase-D1U61Dnt.js";import"./colorManipulator-4CHxWgpo.js";import"./DialogContent-CX0jwU1P.js";import"./Button-6gIpOcku.js";import"./Chip-D4rXNsqT.js";import"./MenuItem-bua5XKnv.js";import"./styled-xzbhM0n_.js";import"./types-CMP91hxZ.js";import"./iframe-CgvgFwUT.js";import"../sb-preview/runtime.js";import"./Edit-DKn8TmSo.js";import"./ThemeProvider-BUQO26gH.js";import"./Remove-B14EzRR3.js";const P=({scrollable:r})=>t(e,{sx:{p:2},children:Array.from({length:7},(k,m)=>t(e,{sx:{mb:1,p:1,borderRadius:1},children:a(T,{children:["Result Item ",m+1]})},m))}),D=()=>{const r=h.useRef(null);return t(u,{ref:r,drawerWidth:177,children:P({scrollable:r!=null&&r.current?r.current.getContainer():window})})},Yt={title:"Component Library/InsetInMap",component:p,decorators:[r=>t(e,{sx:{width:"800px",height:"calc(50vh - 40px)",position:"relative"},children:t(B,{initialMapStyleConfig:A,children:t(r,{})})})],argTypes:{},args:{}},o=r=>t("p",{style:{color:"red"},children:r}),j={topLeft:o("topLeft"),top:o("top"),topRight:o("topRight"),right:o("right"),bottomRight:o("bottomRight"),bottom:o("bottom"),bottomLeft:o("bottomLeft"),left:o("left"),center:o("center")},i={render:r=>t(p,{content:t("p",{style:{opacity:.2,fontSize:"2em"},children:Array(20).fill("CONTENT").join(" ")}),controlArea:j})},n={render:r=>t(p,{content:t(x,{...b}),controlArea:{left:t(D,{}),topRight:t(y,{children:a(L,{direction:"row",spacing:2,alignItems:"center",children:[t(I,{}),t(M,{onClick:()=>alert("Toggle map off")})]})}),bottomRight:t(e,{m:4,children:t(S,{children:t(e,{p:1,sx:{height:44,display:"flex",alignItems:"center",borderRadius:1},children:a(e,{sx:{display:"flex",alignItems:"center",[`& .${w.root}`]:{mx:1}},children:[t(R,{}),t(v,{orientation:"vertical",flexItem:!0}),t(C,{})]})})})})}})};var s,l,c;i.parameters={...i.parameters,docs:{...(s=i.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: args => <InsetInMap content={<p style={{
+    opacity: 0.2,
+    fontSize: '2em'
+  }}>{Array(20).fill("CONTENT").join(" ")}</p>} controlArea={allControlAreas} />
+}`,...(c=(l=i.parameters)==null?void 0:l.docs)==null?void 0:c.source}}};var d,g,f;n.parameters={...n.parameters,docs:{...(d=n.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: args => <InsetInMap {...{
+    content: <BasicMap {...allArgs} />,
+    controlArea: {
+      left: <DrawerWithResults />,
+      topRight: <Container>
+              <Stack direction="row" spacing={2} alignItems="center">
+                <LayerSelectorInsetInMap />
+                <MapToggleButtonPresentational onClick={() => alert("Toggle map off")} />
+              </Stack>
+            </Container>,
+      bottomRight: <Box m={4}>
+              <Paper>
+                <Box p={1} sx={{
+            height: 44,
+            // figma 68 - 2x12
+            display: "flex",
+            alignItems: "center",
+            borderRadius: 1
+          }}>
+                  <Box sx={{
+              display: "flex",
+              alignItems: "center",
+              [\`& .\${dividerClasses.root}\`]: {
+                mx: 1
+              }
+            }}>
+                    <ButtonZoomIn />
+                    <Divider orientation="vertical" flexItem />
+                    <ButtonZoomOut />
+                  </Box>
+                </Box>
+              </Paper>
+            </Box>
+    }
+  }} />
+}`,...(f=(g=n.parameters)==null?void 0:g.docs)==null?void 0:f.source}}};const tr=["AllSlices","MapExample"];export{i as AllSlices,n as MapExample,tr as __namedExportsOrder,Yt as default};
