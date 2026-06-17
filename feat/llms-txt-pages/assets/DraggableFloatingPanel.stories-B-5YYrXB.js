3f9275a1943b9a551c7ef1bbd86f63c91a32057c
ACoolmanTelicent <ACoolmanTelicent@users.noreply.github.com>
2026-06-17 16:01:48 +0000

    deploy: 1b820d37a9a3fb6e07a6af5831e6a2bbd09523e4

---
+++ feat/llms-txt-pages/assets/DraggableFloatingPanel.stories-B-5YYrXB.js
@@ -0,0 +1,35 @@
+import{j as i}from"./emotion-react-jsx-runtime.browser.esm-OTCsP_zj.js";import{M as p}from"./UserIcon-Cn55tEQ2.js";import{K as a,J as m,A as s}from"./DropdownButton-AhMETYip.js";import"./Text-D0lNsqSI.js";import"./Chip-BwAmCpCJ.js";import"./Divider-wVQl8Tit.js";import"./TreeView-CTXybiJL.js";import"./index-7LYIoTbn.js";import{B as l}from"./Box-BVltnmec.js";import"./jsx-runtime-BjG_zV1W.js";import"./createSvgIcon-DU2rVRio.js";import"./defaultTheme-ACnAOKMv.js";import"./styled-Dr6LHSty.js";import"./generateUtilityClasses-CBlNI-gU.js";import"./DefaultPropsProvider-BINl2W1D.js";import"./useTheme-BCfSMqPY.js";import"./useTheme-DmgbyzQG.js";import"./Container-Cpr-ywPw.js";import"./styled-xzbhM0n_.js";import"./createStyled-CsLhtPiI.js";import"./useThemeProps-DhNaUrQ2.js";import"./Stack-_9FQNGpJ.js";import"./Typography-CSCiAqzj.js";import"./Paper-3rHlC0dZ.js";import"./useThemeProps-k0ivW4aO.js";import"./AdapterDayjs-Cl81F8SA.js";import"./index-B4OJPA8G.js";import"./Modal-JNcevMIf.js";import"./utils-CXYPeQrV.js";import"./TransitionGroupContext-itkx9jux.js";import"./useEnhancedEffect-oYjd7u20.js";import"./index-DONRZHi-.js";import"./Select-BfRenVFP.js";import"./useFormControl-lkbi0or6.js";import"./useId-Cf3J66lm.js";import"./GlobalStyles-B130YdEm.js";import"./GlobalStyles-Canp8JzW.js";import"./TextField-BklvXUyq.js";import"./IconButton-K5H0oyhF.js";import"./ButtonBase-D1U61Dnt.js";import"./colorManipulator-4CHxWgpo.js";import"./DialogContent-CX0jwU1P.js";import"./Button-6gIpOcku.js";import"./Chip-D4rXNsqT.js";import"./MenuItem-bua5XKnv.js";import"./dividerClasses-Cw9q1-aC.js";import"./UIThemeProvider-4LUh-fR0.js";import"./types-CMP91hxZ.js";import"./ThemeProvider-BUQO26gH.js";import"./LinearProgress-Bb8hrd7I.js";import"./Spinner-BOxs5Djo.js";import"./Dialog-vILqERHu.js";import"./MapToggleButtonPresentational-86Mslv2I.js";import"./Remove-B14EzRR3.js";import"./TextField-jr6Bp6UA.js";import"./Select-CyedWs7E.js";import"./Switch-CckCM0FO.js";import"./DatePicker-DcCgiJ2j.js";import"./DateTimePicker-Dp4MmL11.js";import"./Divider-A3I7BnBV.js";import"./LinkButton-D9Uz2SFG.js";import"./Paper-CmfYDRfa.js";import"./ErrorFallback-Box0VFpD.js";import"./ErrorFallbackText-BxC3bExW.js";import"./ErrorFallbackWrapper-Dv-Jy562.js";import"./Brand-DN1iXAq-.js";import"./constants-DgMWfE7o.js";import"./iframe-mR7CMK9b.js";import"../sb-preview/runtime.js";import"./Edit-DKn8TmSo.js";const c={DraggablePanel:a},Si={title:"Surfaces/FloatingPanel/FloatingPanel.DraggablePanel",component:c.DraggablePanel,tags:["autodocs"],decorators:n=>i(m.Provider,{value:{panels:{map:{visible:!0,minimised:!1}},toggleVisibility:()=>{},get:()=>!1},children:i(l,{sx:{position:"relative",width:"100%",height:"100vh",overflow:"hidden",bgcolor:"background.default",border:"1px dashed",borderColor:"divider"},children:i(n,{})})})},r={args:{bounds:"parent",title:"Resize baseline",targetId:"map",iconBeforeTitle:i(p,{color:"primary",fontSize:"inherit"}),menu:i(s,{apps:[{icon:"./src/assets/icons/graph.svg",id:"telicent-graph",name:"graph",url:"https://graph.io"},{icon:"./src/assets/icons/search.svg",id:"telicent-search",name:"search",url:"https://search.io"}]}),children:i("div",{style:{height:"100%",display:"flex",flexDirection:"column",minHeight:0},children:i("div",{style:{flex:1,minHeight:0,overflow:"auto",padding:8,backgroundColor:"#C9C9C9"},children:"Resizable content (flex:1). Click on bottom right corner to resize."})})}};var t,o,e;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    bounds: "parent",
+    title: "Resize baseline",
+    targetId: "map",
+    iconBeforeTitle: <MapIcon color="primary" fontSize="inherit" />,
+    menu: <AppSwitch apps={[{
+      icon: "./src/assets/icons/graph.svg",
+      id: "telicent-graph",
+      name: "graph",
+      url: "https://graph.io"
+    }, {
+      icon: "./src/assets/icons/search.svg",
+      id: "telicent-search",
+      name: "search",
+      url: "https://search.io"
+    }]} />,
+    children: <div style={{
+      height: "100%",
+      display: "flex",
+      flexDirection: "column",
+      minHeight: 0
+    }}>
+        <div style={{
+        flex: 1,
+        minHeight: 0,
+        overflow: "auto",
+        padding: 8,
+        backgroundColor: "#C9C9C9"
+      }}>
+          Resizable content (flex:1). Click on bottom right corner to resize.
+        </div>
+      </div>
+  }
+}`,...(e=(o=r.parameters)==null?void 0:o.docs)==null?void 0:e.source}}};const ki=["ResizeBaseline"];export{r as ResizeBaseline,ki as __namedExportsOrder,Si as default};
---
+++ feat/llms-txt-pages/assets/UserProfileContent.stories-zXqG3SBs.js
@@ -0,0 +1,27 @@
+import{a as o,j as t,F as x}from"./emotion-react-jsx-runtime.browser.esm-OTCsP_zj.js";import{a as g,B as a}from"./DropdownButton-AhMETYip.js";import{a as m}from"./UserIcon-Cn55tEQ2.js";import{T as n}from"./Text-D0lNsqSI.js";import"./jsx-runtime-BjG_zV1W.js";import"./index-7LYIoTbn.js";import"./useThemeProps-k0ivW4aO.js";import"./defaultTheme-ACnAOKMv.js";import"./useThemeProps-DhNaUrQ2.js";import"./DefaultPropsProvider-BINl2W1D.js";import"./useTheme-DmgbyzQG.js";import"./useTheme-BCfSMqPY.js";import"./AdapterDayjs-Cl81F8SA.js";import"./styled-Dr6LHSty.js";import"./index-B4OJPA8G.js";import"./Typography-CSCiAqzj.js";import"./generateUtilityClasses-CBlNI-gU.js";import"./Modal-JNcevMIf.js";import"./utils-CXYPeQrV.js";import"./TransitionGroupContext-itkx9jux.js";import"./useEnhancedEffect-oYjd7u20.js";import"./index-DONRZHi-.js";import"./Select-BfRenVFP.js";import"./useFormControl-lkbi0or6.js";import"./Paper-3rHlC0dZ.js";import"./useId-Cf3J66lm.js";import"./createSvgIcon-DU2rVRio.js";import"./GlobalStyles-B130YdEm.js";import"./GlobalStyles-Canp8JzW.js";import"./TextField-BklvXUyq.js";import"./createStyled-CsLhtPiI.js";import"./IconButton-K5H0oyhF.js";import"./ButtonBase-D1U61Dnt.js";import"./colorManipulator-4CHxWgpo.js";import"./DialogContent-CX0jwU1P.js";import"./Button-6gIpOcku.js";import"./Chip-D4rXNsqT.js";import"./MenuItem-bua5XKnv.js";import"./dividerClasses-Cw9q1-aC.js";import"./UIThemeProvider-4LUh-fR0.js";import"./types-CMP91hxZ.js";import"./ThemeProvider-BUQO26gH.js";import"./Chip-BwAmCpCJ.js";import"./Divider-wVQl8Tit.js";import"./Divider-A3I7BnBV.js";import"./Box-BVltnmec.js";import"./TreeView-CTXybiJL.js";import"./Stack-_9FQNGpJ.js";import"./styled-xzbhM0n_.js";import"./LinearProgress-Bb8hrd7I.js";import"./Spinner-BOxs5Djo.js";import"./Dialog-vILqERHu.js";import"./MapToggleButtonPresentational-86Mslv2I.js";import"./Remove-B14EzRR3.js";import"./TextField-jr6Bp6UA.js";import"./Select-CyedWs7E.js";import"./Switch-CckCM0FO.js";import"./DatePicker-DcCgiJ2j.js";import"./DateTimePicker-Dp4MmL11.js";import"./Container-Cpr-ywPw.js";import"./LinkButton-D9Uz2SFG.js";import"./Paper-CmfYDRfa.js";import"./ErrorFallback-Box0VFpD.js";import"./ErrorFallbackText-BxC3bExW.js";import"./ErrorFallbackWrapper-Dv-Jy562.js";import"./Brand-DN1iXAq-.js";import"./constants-DgMWfE7o.js";import"./iframe-mR7CMK9b.js";import"../sb-preview/runtime.js";import"./Edit-DKn8TmSo.js";const Dt={title:"Data display/User profile/User profile content",component:g,tags:["autodocs"],decorators:i=>t("div",{style:{width:300},children:t(i,{})}),argTypes:{children:{control:!1,description:"Content inside the component",table:{type:{summary:"ReactNode"}}}}},u=[{heading:"Username",value:"Admin"},{heading:"Email",value:"admin@telicent.io"},{heading:"Deployed Organisation",value:"Telicent"},{heading:"UserId",value:"3004958Ed-23-sdf-404-21334"},{heading:"Attributes",value:"GBR"},{heading:"Clearance",value:"SEN"}],r={args:{children:u.slice(0,3).map(i=>o(m,{children:[t(n,{children:t("b",{children:i.heading})}),t(n,{children:i.value})]},i.heading))}},e={args:{children:o(x,{children:[u.map(i=>o(m,{children:[t(n,{children:t("b",{children:i.heading})}),t(n,{children:i.value})]},i.heading)),o(m,{direction:"row",columnGap:2,justifyContent:"end",children:[t(a,{variant:"secondary",disableElevation:!0,children:"Action"}),t(a,{disableElevation:!0,children:"Action"})]})]})}};var p,s,d;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    children: items.slice(0, 3).map(item => <FlexBox key={item.heading}>
+        <Text>
+          <b>{item.heading}</b>
+        </Text>
+        <Text>{item.value}</Text>
+      </FlexBox>)
+  }
+}`,...(d=(s=r.parameters)==null?void 0:s.docs)==null?void 0:d.source}}};var l,c,h;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    children: <>
+        {items.map(item => <FlexBox key={item.heading}>
+            <Text>
+              <b>{item.heading}</b>
+            </Text>
+            <Text>{item.value}</Text>
+          </FlexBox>)}
+        <FlexBox direction="row" columnGap={2} justifyContent="end">
+          <Button variant="secondary" disableElevation>
+            Action
+          </Button>
+          <Button disableElevation>Action</Button>
+        </FlexBox>
+      </>
+  }
+}`,...(h=(c=e.parameters)==null?void 0:c.docs)==null?void 0:h.source}}};const Nt=["Basic","WithActions"];export{r as Basic,e as WithActions,Nt as __namedExportsOrder,Dt as default};
